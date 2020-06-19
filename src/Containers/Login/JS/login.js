9d85ca0cd61ca664a029925ab8a841bdbe5fd1c7
Daniel Ngozika <danielngozikac@gmail.com>
2020-06-19 09:41:38 +0100

    Create dummy login page.

---
+++ src/Containers/Login/JS/login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from "../CSS/login.module.css";
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    render() {
+        return <h3>Login Page</h3>
+    }
+}
+
+export default Login;
