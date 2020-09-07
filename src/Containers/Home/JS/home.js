08e35ddac838f6629121b4666f6593dc527b4e47
Daniel Ngozika <danielngozikac@gmail.com>
2020-09-07 16:38:14 +0100

    Create Home page.

---
+++ src/Containers/Home/JS/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Layout from "../../../Components/UI/Layout/JS/layout";
+import styles from "../CSS/home.module.css";
+
+class Home extends Component {
+	render() {
+		return <Layout />;
+	}
+}
+
+export default Home;
