80e4f469212852ca7648e62a33815fc659cd191a
iheanyi25 <ihekwebai@gmail.com>
2020-10-27 11:44:01 +0100

    added the create and edit health plan pages and linked them

--- src/Pages/Admin/Util_HealtlPlans/EditHealthPlan.js
+++ src/Pages/Admin/Util_HealtlPlans/EditHealthPlan.js
@@ -1,12 +1,98 @@
-
 import React, { Component } from 'react'
+import { PageLoader, TemplateSettings } from '../../../Components';
 
 export default class EditHealthPlan extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+				<PageLoader />
+				<main className="main-content">
+					<div className="app-loader">
+						<i className="icofont-spinner-alt-4 rotate" />
+					</div>
+					<div className="main-content-wrap w-50">
+						<div className="page-content">
+							<div className="row justify-content-center">
+								<div className="col col-md-12">
+									<div class="card border-light">
+										<div class="card-body">
+											<form className="mb-4 p-5">
+												<h4 className="text-center">Edit a health plan</h4>
+												<div className="form-group">
+													<label>Name</label>
+													<input
+														className="form-control"
+														type="text"
+														tabIndex={-98}
+														placeholder="Name of the health plan"
+													/>
+												</div>
+												<div className="form-group">
+													<label>Cost</label>
+													<input
+														className="form-control"
+														type="number"
+														tabIndex={-98}
+														placeholder="Price of the health plan"
+													/>
+												</div>
+												<div className="form-group">
+													<label>Renewal Cost</label>
+													<input
+														className="form-control"
+														type="number"
+														tabIndex={-98}
+														placeholder="Cost of annual renewal of card"
+													/>
+												</div>
+												<div className="form-group">
+													<label>Patients Per Folder</label>
+													<input
+														className="form-control"
+														type="number"
+														tabIndex={-98}
+														placeholder="Number of patient per folder"
+													/>
+												</div>
+												<div className="form-group">
+													<label>Accounts Per Health Plan</label>
+													<input
+														className="form-control"
+														type="number"
+														tabIndex={-98}
+														placeholder="Accounts Per health plan"
+													/>
+												</div>
+												<div className="form-group">
+													<div class="custom-control custom-switch mb-3">
+														<input
+															type="checkbox"
+															class="custom-control-input"
+															id="control2"
+														/>{' '}
+														<label class="custom-control-label" for="control2">
+															Instant Billing
+														</label>
+													</div>
+												</div>
+												<div className="row">
+													<div className="col"></div>
+													<div className="col text-right">
+														<button type="button" className="btn btn-primary">
+															Submit
+														</button>
+													</div>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+				<TemplateSettings />
+			</>
         )
     }
 }
--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -20,12 +20,10 @@ import ManageServiceCategory from '../Pages/Admin/Util_Services//ManageServiceCa
 import ManageServices from '../Pages/Admin/Util_Services//ManageServices';
 import EditServiceCategory from '../Pages/Admin/Util_Services//EditServiceCategory';
 import EditService from '../Pages/Admin/Util_Services//EditService';
-import CreateWard from '../Pages/Admin/Util_Ward/CreateWard';
-import EditWard from '../Pages/Admin/Util_Ward/EditWard';
 
 // ward utils
-import CreateWard from "../Pages/Admin/Util_Ward/CreateWard";
-import EditWard from "../Pages/Admin/Util_Ward/EditWard";
+import CreateWard from '../Pages/Admin/Util_Ward/CreateWard';
+import EditWard from '../Pages/Admin/Util_Ward/EditWard';
 import ManageWards from "../Pages/Admin/Util_Ward/ManageWards";
 
 // health-plan utils
