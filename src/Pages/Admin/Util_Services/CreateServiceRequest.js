194b2bc7d8db3cbe54367cfda31fb2ebbf527b8b
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2020-11-03 12:10:38 +0100

    create a service screen created

---
+++ src/Pages/Admin/Util_Services/CreateServiceRequest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateServiceRequest extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
