21a8491dfff3b9cf48e0569b2c8e7c2e90264944
iheanyi10 <i.ihekweaba@genesystechhub.com>
2020-11-04 14:08:42 +0100

    successfully seperated concerns in patient registeration

---
+++ src/Pages/Admin/Component/PayCash.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const PayCash = () => {
+  return (
+    <div className="table-responsive">
+    <div className="main-content-wrap w-50">
+      <div className="page-content">
+        <div className="row justify-content-center">
+          <div className="col col-md-12">
+            <div className="card border-light">
+              <div className="card-body">
+                <form
+                  className="mb-4 p-5 needs-validation"
+                  noValidate
+                >
+                  <div className="form-group">
+                    <label>Amount(NGN)</label>
+                    <input
+                      className="form-control"
+                      type="number"
+                      tabIndex={-98}
+                      name="name"
+                      defaultValue="5000"
+                      required
+                    />
+                    <div className="valid-feedback">
+                      Looks good!
+                    </div>
+                    <div className="invalid-feedback">
+                      Oops! should be numbers only.
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label>Comment</label>
+                    <textarea
+                      className="form-control"
+                      rows={3}
+                      name="description"
+                      required
+                    />
+                    <div className="valid-feedback">
+                      Looks good!
+                    </div>
+                    <div className="invalid-feedback">
+                      Enter a valid comment
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col"></div>
+                    <div className="col text-right">
+                      <button
+                        type="submit"
+                        className="btn btn-primary"
+                      >
+                        Pay now
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export { PayCash };
