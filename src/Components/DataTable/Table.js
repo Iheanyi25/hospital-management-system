d31b5e5b5dbe6f168f77b14a8862ee4a40986801
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2020-11-07 16:59:29 +0100

    reusable datatable component

---
+++ src/Components/DataTable/Table.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react'
+
+let $ = undefined
+let interval = undefined;
+let tableID = Math.random();
+tableID = tableID.toString().replace(".", "_");
+
+const Table = ({ content }) => {
+
+    useEffect(() => {
+        setJquery();
+    }, []);
+
+    const sync = () => {
+        let element = $(`#custom_table_${tableID}`);
+        element.DataTable();
+    }
+
+    const setJquery = () => {
+        interval = setInterval(() => {
+            if (window.$ && window.$.prototype.DataTable) {
+                clearInterval(interval)
+                $ = window.$;
+                sync()
+            }
+        }, 1000);
+    }
+
+    const formatContent = () => {
+        let headers = Object.keys(content[0]).map((item, index) => <th key={index}>{item}</th>);
+        let body = content.map((item, i) => {
+            return <tr key={i}>
+                {Object.values(item).map((currentValue, index) => (
+                    <td key={index}>{currentValue}</td>
+                ))}
+            </tr>
+        })
+
+        return (
+            <>
+                <thead>
+                    <tr>
+                        {headers}
+                    </tr>
+                </thead>
+                <tbody>
+                    {body}
+                </tbody>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div className="table-responsive">
+                <table
+                    className="table table-striped"
+                    data-paging="true"
+                    data-info="true"
+                    data-searching="true"
+                    id={`custom_table_${tableID}`}
+                >
+                    {formatContent()}
+                </table>
+            </div>
+        </>
+    )
+}
+
+export { Table }
