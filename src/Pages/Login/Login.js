7a15bdcf2b36af2dc12810feb7a69f7d0ee01b98
iheanyi10 <i.ihekweaba@genesystechhub.com>
2020-11-11 12:43:23 +0100

    refactored and added an alert component to this component

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,114 +1,60 @@
 import React, { Component } from "react";
 import styles from "./css/Login.module.css";
+import { InvalidDetails } from "../../Components/Alerts";
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    apiUrl: process.env.REACT_APP_API_URL,
+    email: "",
+    password: "",
+    submitting: false,
+    error: false,
+  };
 
-    this.state = {
-      apiUrl: process.env.REACT_APP_API_URL,
-      email: "",
-      password: "",
-      errorMessage: "",
-      showErrorMessage: false,
-      submitting: false,
-    };
-
-    this.login = this.login.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(name, e) {
-    const value = e.target.value;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  async login(e) {
-    e.preventDefault();
+  handleSubmit = async (e) => {
     this.setState({ submitting: true });
-    const { email, password } = this.state;
-    console.log(email);
-    console.log(password);
+    e.preventDefault();
     const url = this.state.apiUrl;
+    const { email, password } = this.state;
+    const data = { email, password };
+    console.log(data);
+    if (email !== "" && password !== "") {
+      try {
+        let res = await fetch(`${url}/Auth/Login`, {
+          headers: { "Content-Type": "application/json-patch+json" },
+          method: "POST",
+          body: JSON.stringify(data),
+          redirect: "follow",
+        });
+        if (res.status === 200) {
+          console.log(res);
+          const data = await res.json();
+          console.log(data);
+          localStorage.setItem("token", data.token);
+          localStorage.setItem(
+            "authenticatedUser",
+            JSON.stringify(data.authenticatedUser)
+          );
 
-    try {
-      const request = await fetch(`${url}/Auth/Login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-
-      if (!request.ok) {
-        console.log("kfgkgkgfk: ", request.json());
-        const error = await request.json();
-        this.setState({ submitting: false });
-        throw Error(error.message);
-      }
-
-      const data = await request.json();
-
-      localStorage.setItem("token", data.token);
-      localStorage.setItem(
-        "authenticatedUser",
-        JSON.stringify(data.authenticatedUser)
-      );
-
-      window.location.reload();
-
-      // if (data.authenticatedUser.userType === "Admin" || data.authenticatedUser.userType === "admin") {
-      // 	this.props.history.push("/AdminDashboard");
-
-      // } else if (data.authenticatedUser.userType === "Patient" || data.authenticatedUser.userType === "patient") {
-      // 	this.props.history.push("/PatientDashboard");
-
-      // } else if (data.authenticatedUser.userType === "Doctor" || data.authenticatedUser.userType === "doctor") {
-      // 	this.props.history.push("/DoctorDashboard");
-
-      // } else if (data.authenticatedUser.userType === "Accountant" || data.authenticatedUser.userType === "accountant") {
-      // 	this.props.history.push("/AccountantDashboard");
-
-      // } else if (data.authenticatedUser.userType === "Pharmacy" || data.authenticatedUser.userType === "pharmacy") {
-      // 	this.props.history.push("/PharmacyDashboard");
-
-      // } else if (data.authenticatedUser.userType === "Lab" || data.authenticatedUser.userType === "lab") {
-      // 	this.props.history.push("/LabDashboard");
-      // }
-    } catch (err) {
-      console.log(err.message);
-      this.setState({
-        ...this.state,
-        showErrorMessage: true,
-        errorMessage: err.message,
-        submitting: !this.state.submitting,
-      });
+          window.location.reload();
+        } else {
+          console.log(res);
+          this.setState({ error: true, submitting: false, password: ""});
+        }
+      } catch (error) {}
     }
+  };
+
+  setErrorStatus =()=>{
+    this.setState({ error: false });
   }
 
   render() {
-    const { email, password, submitting } = this.state;
-    var displayError;
-
-    if (this.state.showErrorMessage) {
-      displayError = (
-        <div className="alert alert-warning with-after-icon" role="alert">
-          <div className="alert-content">{this.state.errorMessage}</div>
-          <div className="alert-icon">
-            <i className="icofont-alarm" />
-          </div>
-        </div>
-      );
-    }
-
+    const { submitting, error, email, password } = this.state;
     return (
       <>
         <div className={styles.background}>
+          {error ? <InvalidDetails setErrorStatus={this.setErrorStatus}/> : null}
           <div className={styles.div}>
             <h1>
               <img
@@ -121,7 +67,11 @@ class Login extends Component {
               Hospital Management Solution
             </h1>
             <h2>Login</h2>
-            <form className={styles.form} onSubmit={(e) => this.login(e)}>
+            <form
+              className={`${styles.form} needs-validation`}
+              noValidate
+              onSubmit={(e) => this.handleSubmit(e)}
+            >
               <div className="form-group">
                 <label>Email Address</label>
                 <input
@@ -129,10 +79,16 @@ class Login extends Component {
                   type="email"
                   name="email"
                   value={email}
-                  onChange={(e) => this.handleChange("email", e)}
+                  onChange={(e) => {
+                    this.setState({ [e.target.name]: e.target.value });
+                  }}
                   placeholder="Your Email Address"
                   required
                 />
+                <div className="valid-feedback">Looks good!</div>
+                <div className="invalid-feedback">
+                  Please provide a valid email.
+                </div>
               </div>
 
               <div className="form-group">
@@ -142,14 +98,17 @@ class Login extends Component {
                   type="password"
                   name="password"
                   value={password}
-                  onChange={(e) => this.handleChange("password", e)}
+                  onChange={(e) => {
+                    this.setState({ [e.target.name]: e.target.value });
+                  }}
                   placeholder="Your Password"
                   required
                 />
+                <div className="valid-feedback">Looks good!</div>
+                <div className="invalid-feedback">
+                  Please provide a valid password.
+                </div>
               </div>
-
-              {displayError}
-
               <button
                 className="btn btn-primary"
                 type="submit"
