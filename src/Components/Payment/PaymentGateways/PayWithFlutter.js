832752ed51f6d68f56abe09dd0462594093f4a2b
iheanyi10 <i.ihekweaba@genesystechhub.com>
2020-11-12 12:55:37 +0100

    cleared unnecessary code

--- src/Components/Payment/PaymentGateways/PayWithFlutter.js
+++ src/Components/Payment/PaymentGateways/PayWithFlutter.js
@@ -3,10 +3,7 @@ import { useRavePayment } from "react-ravepayment";
 import flutterwave1 from "../../../assets/img/flutterwave1.svg";
 import flutterwave2 from "../../../assets/img/flutterwave2.svg";
 
-const apiUrl = process.env.REACT_APP_API_URL;
-
-const PayWithFlutter = ({ paymentDetails, handleSuccess, paidSuccessfully }) => {
-  let userType = JSON.parse(localStorage.getItem("authenticatedUser")).userType;
+const PayWithFlutter = ({ paymentDetails, paidSuccessfully }) => {
   const [details, setDetails] = useState({
     txref: "rave-123456",
     customer_email: "",
@@ -32,34 +29,7 @@ const PayWithFlutter = ({ paymentDetails, handleSuccess, paidSuccessfully }) => 
 
   const onSuccess = (reference) => {
     paidSuccessfully(reference, "online-flutterwave");
-    // handleSubmit(reference);
   };
-
-  // const handleSubmit = async (reference) => {
-  //   let payload = {
-  //     amount: paymentDetails.amount,
-  //     [`${
-  //       userType === "Admin" ? "accountId" : "patientId"
-  //     }`]: paymentDetails.patientId,
-  //     modeOfPayment: "online-flutterwave",
-  //     transactionRefrence: reference.data.data.orderRef,
-  //     paymentDescription: paymentDetails.paymentDescription,
-  //   };
-  //   console.log(payload);
-  //   try {
-  //     let res = await fetch(`${apiUrl}/${userType}/Account/FundAccount`, {
-  //       headers: { "Content-Type": "application/json-patch+json" },
-  //       method: "POST",
-  //       body: JSON.stringify(payload),
-  //       redirect: "follow",
-  //     });
-  //     if (res.status === 200) {
-  //       handleSuccess(true);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
 
   const onClose = () => {
     console.log("closed");
@@ -71,9 +41,6 @@ const PayWithFlutter = ({ paymentDetails, handleSuccess, paidSuccessfully }) => 
         className="btn btn-light btn-lg btn-block"
         name="modeOfPayment"
         value="paystack"
-        // onClick={(e) => {
-        //   handlePayment(e, initializePayment, details);
-        // }}
         onClick={(e) => handlePayment(e)}
       >
         <img src={flutterwave1} className="mr-1" alt="" />
--- src/Components/Payment/PaymentGateways/PayWithPaystack.js
+++ src/Components/Payment/PaymentGateways/PayWithPaystack.js
@@ -3,13 +3,7 @@ import paystack1 from "../../../assets/img/paystack-icon1.svg";
 import paystack2 from "../../../assets/img/paystack-icon2.svg";
 import { usePaystackPayment } from "react-paystack";
 
-const PayWithPaystack = ({
-  paymentDetails,
-  handleSuccess,
-  paidSuccessfully,
-}) => {
-  let userType = JSON.parse(localStorage.getItem("authenticatedUser")).userType;
-
+const PayWithPaystack = ({ paymentDetails, paidSuccessfully }) => {
   const [details, setDetails] = useState({
     reference: new Date().getTime(),
     email: "",
@@ -33,51 +27,7 @@ const PayWithPaystack = ({
   };
   const onSuccess = (reference) => {
     paidSuccessfully(reference, "online-paystack");
-    // if (paymentDetails.fundAccount) {
-    //   payForServices(reference);
-    //   let payload = {
-    //     amount: paymentDetails.amount,
-    //     patientId: paymentDetails.patientId,
-    //     serviceRequestId: paymentDetails.serviceRequestId,
-    //     modeOfPayment: "online-paystack",
-    //     referenceNumber: reference.trxref,
-    //     paymentDescription: paymentDetails.paymentDescription,
-    //   };
-    //   console.log(payload);
-    // } else {
-    //   fundAccount(reference);
-    // }
   };
-
-  // const payForServices = async (reference) => {
-  //   console.log(reference);
-  // };
-
-  // const fundAccount = async (reference) => {
-  //   let payload = {
-  //     amount: paymentDetails.amount,
-  //     [`${
-  //       userType === "Admin" ? "accountId" : "patientId"
-  //     }`]: paymentDetails.patientId,
-  //     modeOfPayment: "online-paystack",
-  //     transactionRefrence: reference.trxref,
-  //     paymentDescription: paymentDetails.paymentDescription,
-  //   };
-  //   console.log(payload);
-  //   try {
-  //     let res = await fetch(`${apiUrl}/${userType}/Account/FundAccount`, {
-  //       headers: { "Content-Type": "application/json-patch+json" },
-  //       method: "POST",
-  //       body: JSON.stringify(payload),
-  //       redirect: "follow",
-  //     });
-  //     if (res.status === 200) {
-  //       handleSuccess(true);
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
 
   const onClose = () => {
     console.log("closed");
