1e98f826935b7d4bd3cc5a434c999fec2369719c
iheanyi10 <i.ihekweaba@genesystechhub.com>
2020-11-17 13:57:39 +0100

    integrated this page with the endpoint

--- src/Pages/Admin/ViewPreConsultationHistory.js
+++ src/Pages/Admin/ViewPreConsultationHistory.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PageLoader } from "../../Components";
+import formatDate from '../../utils/formatDate'
 
 let $ = window.$;
 $.DataTables = require("datatables.net");
@@ -7,7 +8,38 @@ $.DataTables = require("datatables.net");
 const apiUrl = process.env.REACT_APP_API_URL;
 
 class ViewPreConsultationHistory extends React.Component {
+  state = {
+    patientPreConsultations: [],
+  };
+
+  componentDidMount() {
+    this.fetchServiceCategories();
+  }
+
+  fetchServiceCategories = async () => {
+    const { id } = this.props.match.params;
+    try {
+      let res = await fetch(
+        `${apiUrl}/PatientPreConsultation/GetPatientPreConsultation?PatientId=${id}`,
+        {
+          headers: { "Content-Type": "application/json-patch+json" },
+          method: "GET",
+          redirect: "follow",
+        }
+      );
+      const data = await res.text();
+      console.log(JSON.parse(data));
+      this.setState({
+        patientPreConsultations: JSON.parse(data).patientPreConsultation,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
+    console.log(this.props.history.location.state);
+    const { firstName, lastName} =this.props.history.location.state
     return (
       <>
         <PageLoader />
@@ -18,95 +50,84 @@ class ViewPreConsultationHistory extends React.Component {
           </div>
           <div className="main-content-wrap">
             <div className="page-content">
-              <div className="card border-light w-50 m-auto">
+              <div className="card border-light w-75 m-auto">
                 <div className="card-body">
                   <h4 className="text-center mb-4">
-                    Patient’s preconsultation health history
+                    {`${firstName} ${lastName}’s pre-consultation health history`}
                   </h4>
-                  <div id="accordion">
-                    <div className="card-header" id="headingTwo">
-                      <h5 className="mb-0">
-                        <button
-                          className="btn btn-outline-primary btn-block"
-                          data-toggle="collapse"
-                          data-target="#collapseOne"
-                          aria-expanded="true"
-                          aria-controls="collapseOne"
-                        >
-                          Captured 12 Dec, 2020
-                        </button>
-                      </h5>
-                    </div>
-                    <div
-                      id="collapseOne"
-                      className="collapse show"
-                      aria-labelledby="headingOne"
-                      data-parent="#accordion"
-                    >
-                      <div className="card-body w-50">
-                        <div className="pl-5">
-                          <h6 className="font-weight-bold">Patient Vitals</h6>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0">Blood pressure</p>
-                              <small className="text-info">120/80</small>
+                  <div id="accordion" className="mb-3">
+                    {this.state.patientPreConsultations.length === 0 ?<h5 className="text-center mt-5">Nothing to see here</h5>: this.state.patientPreConsultations?.map(
+                      (patientPreConsultation, index) => (
+                        <div className="card mb-0">
+                          <div className="card-header" id={`heading${index + 1}`}>
+                            <h5 className="mb-0">
+                              <button
+                                className="btn btn-outline-primary btn-block"
+                                data-toggle="collapse"
+                                data-target={`#collapse${index + 1}`}
+                                aria-expanded="true"
+                                aria-controls={`collapse${index + 1}`}
+                              >
+                                {`Captured on ${formatDate(patientPreConsultation?.date) ?? ""}`}
+                              </button>
+                            </h5>
                           </div>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0 mt-2">Respiration</p>
-                              <small className="text-info">16</small>
-                          </div>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0 mt-2">Pulse</p>
-                              <small className="text-info">100</small>
-                          </div>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0 mt-2">SPO2</p>
-                              <small className="text-info">3,000</small>
-                          </div>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0 mt-2">Tempreture (celcius)</p>
-                              <small className="text-info">36</small>
+                          <div
+                            id={`collapse${index + 1}`}
+                            className="collapse"
+                            aria-labelledby="headingOne"
+                            data-parent="#accordion"
+                          >
+                            <div className="card-body w-50">
+                              <div className="pl-5">
+                                <h6 className="font-weight-bold">
+                                  Patient Vitals
+                                </h6>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0">Blood pressure</p>
+                                  <small className="text-info">{patientPreConsultation?.bloodPressure}</small>
+                                </div>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0 mt-2">Respiration</p>
+                                  <small className="text-info">{patientPreConsultation?.respiration}</small>
+                                </div>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0 mt-2">Pulse</p>
+                                  <small className="text-info">{patientPreConsultation?.pulse}</small>
+                                </div>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0 mt-2">SPO2</p>
+                                  <small className="text-info">{patientPreConsultation?.spO2}</small>
+                                </div>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0 mt-2">
+                                    Tempreture (celcius)
+                                  </p>
+                                  <small className="text-info">{patientPreConsultation?.temperature}</small>
+                                </div>
+                              </div>
+                              <div className="pl-5 mt-4">
+                                <h6 className="font-weight-bold">
+                                  Patient BMI
+                                </h6>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0">Weight (kg)</p>
+                                  <small className="text-info">{patientPreConsultation?.weight}</small>
+                                </div>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0 mt-2">Height (m)</p>
+                                  <small className="text-info">{patientPreConsultation?.height}</small>
+                                </div>
+                                <div className="border-bottom pb-3">
+                                  <p className="mb-0 mt-2">Calculated BMI</p>
+                                  <small className="text-info">{patientPreConsultation?.calculatedBMI}</small>
+                                </div>
+                              </div>
+                            </div>
                           </div>
                         </div>
-                        <div className="pl-5 mt-4">
-                          <h6 className="font-weight-bold">Patient BMI</h6>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0">Weight (kg)</p>
-                              <small className="text-info">78</small>
-                          </div>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0 mt-2">Height (m)</p>
-                              <small className="text-info">192</small>
-                          </div>
-                          <div className="border-bottom pb-3">
-                              <p className="mb-0 mt-2">Calculated BMI</p>
-                              <small className="text-info">30 </small>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="card-header" id="headingTwo">
-                      <h5 className="mb-0">
-                        <button
-                          className="btn btn-primary btn-block collapsed"
-                          data-toggle="collapse"
-                          data-target="#collapseTwo"
-                          aria-expanded="false"
-                          aria-controls="collapseTwo"
-                        >
-                          Captured 12 Dec, 2020
-                        </button>
-                      </h5>
-                    </div>
-                    <div
-                      id="collapseTwo"
-                      className="collapse"
-                      aria-labelledby="headingTwo"
-                      data-parent="#accordion"
-                    >
-                      <div className="card-body">
-                        
-                      </div>
-                    </div>
+                      )
+                    ) ?? null}
                   </div>
                 </div>
               </div>
