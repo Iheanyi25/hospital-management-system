c01514708fa1b9583a1d2c045692aa9463f9de5d
iheanyi10 <i.ihekweaba@genesystechhub.com>
2020-11-18 15:00:04 +0100

    made the view lab result viewable from clarking and view results

--- src/Pages/Admin/Util_Services/ViewLabResults.js
+++ src/Pages/Admin/Util_Services/ViewLabResults.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { PageLoader } from "../../../Components";
 import resultImage from "../../../assets/img/resultImage.svg";
+import { LabResults } from "../../../Components/Clarking";
 
 let $ = window.$;
 $.DataTables = require("datatables.net");
@@ -35,6 +36,7 @@ class ViewLabResults extends React.Component {
   };
 
   render() {
+    const { id } = this.props.match.params;
     return (
       <>
         <PageLoader />
@@ -44,9 +46,9 @@ class ViewLabResults extends React.Component {
             <i className="icofont-spinner-alt-4 rotate" />
           </div>
           <div className="main-content-wrap">
-            <header className="page-header justify-content-between d-flex align-items-center mb-2">
+            {/* <header className="page-header justify-content-between d-flex align-items-center mb-2">
               <h4 className="page-title">Results for (lab) services</h4>
-            </header>
+            </header> */}
             <div className="row">
               <div className="col col-12 col-md-6 col-xl-4">
                 <div className="card animated fadeInUp delay-02s bg-light">
@@ -68,94 +70,9 @@ class ViewLabResults extends React.Component {
                 </div>
               </div>
             </div>
-
-            <div className="page-content">
-              <div className="card-body"></div>
-            </div>
             <div className="page-content">
               <div className="card border-light w-75 m-auto">
-                <div className="card-body">
-                  <h4 className="text-center mb-4">
-                    Results of (lab) services
-                  </h4>
-                  <div id="accordion" className="mb-3">
-                    {this.state.serviceRequestResults.length === 0 ? <h5 className="text-center mt-5">Nothing to see here</h5>: this.state.serviceRequestResults.map(
-                      (serviceRequestResult, index) => (
-                        <div className="card mb-0">
-                          <div
-                            className="card-header"
-                            id={`heading${index + 1}`}
-                          >
-                            <h5 className="mb-0">
-                              <button
-                                className="btn btn-outline-primary btn-block"
-                                data-toggle="collapse"
-                                data-target={`#collapse${index + 1}`}
-                                aria-expanded="true"
-                                aria-controls={`collapse${index + 1}`}
-                              >
-                                {`${serviceRequestResult.serviceRequest?.service?.name}`}
-                              </button>
-                            </h5>
-                          </div>
-                          <div
-                            id={`collapse${index + 1}`}
-                            className="collapse"
-                            aria-labelledby="headingOne"
-                            data-parent="#accordion"
-                          >
-                            <div className="card-body">
-                              <div>
-                                <h5 className="m-0">{`${serviceRequestResult.serviceRequest?.service?.name}`}</h5>
-                                <h6 className="mt-1">{`${serviceRequestResult.serviceRequest?.service?.serviceCategory?.name}`}</h6>
-                              </div>
-                              <div className="m-auto d-flex flex-wrap">
-                                <div className="mr-2 mb-2">
-                                  <img
-                                    src={resultImage}
-                                    style={{ height: "200px", width: "200px" }}
-                                    alt="result"
-                                  />
-                                </div>
-                                <div className="mr-2 mb-2">
-                                  <img
-                                    src={resultImage}
-                                    style={{ height: "200px", width: "200px" }}
-                                    alt="result"
-                                  />
-                                </div>
-                                <div className="mr-2 mb-2">
-                                  <img
-                                    src={resultImage}
-                                    style={{ height: "200px", width: "200px" }}
-                                    alt="result"
-                                  />
-                                </div>
-                              </div>
-                              <div className="result">
-                                <h5>Result</h5>
-                                <p>{`${serviceRequestResult.result}`}</p>
-                              </div>
-                              <div className="comments">
-                                <h5>Additional comments</h5>
-                                <p>{`${serviceRequestResult.additionalComments}`}</p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      )
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="add-action-box">
-                <button
-                  className="btn btn-primary btn-lg btn-square rounded-pill"
-                  data-toggle="modal"
-                  data-target="#add-appointment"
-                >
-                  <span className="btn-icon icofont-stethoscope-alt" />
-                </button>
+                <LabResults serviceRequestId={id}/>
               </div>
             </div>
           </div>
