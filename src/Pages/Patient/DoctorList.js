84df54e95d5ca0fd91c45cdce8fd09f619a241e3
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2020-11-19 15:43:41 +0100

    doctor lists design

--- src/Pages/Patient/DoctorList.js
+++ src/Pages/Patient/DoctorList.js
@@ -47,25 +47,23 @@ class DoctorList extends React.Component {
                                         <div className="contact">
                                             <div className="img-box"><img src="../assets/content/doctor-400-1.jpg" width={400} height={400} alt="Hello" /></div>
                                             <div className="info-box">
-                                                <h4 className="name">Dr. {doctor.doctor.firstName} {doctor.doctor.lastName}</h4>
+                                                <Link className="name text-primary nameLink" to={`/PatientDoctorProfile/${doctor.doctorId}`}>Dr. {doctor.doctor.lastName} {doctor.doctor.firstName}</Link>
                                                 <p className="role">{doctor?.specialization || ""}</p>
                                                 <div className="custom-control custom-switch mb-3">
                                                     <input type="checkbox" className="custom-control-input" defaultChecked={doctor?.isAvaliable ? 'checked' : ' '} />
                                                     <label className="custom-control-label">Avalible for Consultation</label>
                                                 </div>
                                                 <p className="address">{doctor?.bio || ""}</p>
-                                                <div className="button-box">
-
-                                                    <Link className="btn btn-primary mr-2" to={`/PatientDoctorProfile/${doctor.doctorId}`}>
+                                                <div className="button-box row">
+                                                    <Link style={{ fontSize: "0.9em" }} className="btn btn-outline-primary mr-2" to={`/PatientBookAppointment/${doctor.doctorId}`}>
                                                         <span className="link-icon icofont-doctor" />
-                                                        <span className="link-text">View profile</span>
+                                                        <span className="link-text">Book Appointment</span>
                                                     </Link>
 
-                                                    <Link className="btn btn-info" to={`/PatientBookConsultation/${doctor.doctorId}`}>
+                                                    <Link style={{ fontSize: "0.8em" }} className="btn btn-primary" to={`/PatientBookConsultation/${doctor.doctorId}`}>
                                                         <span className="link-icon icofont-doctor" />
                                                         <span className="link-text">Book Consultation</span>
                                                     </Link>
-
                                                 </div>
 
                                             </div>
