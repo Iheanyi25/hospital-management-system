e20a43a3eced6f3ef79beb6cad66c6d9c8ca3b5d
iheanyi10 <i.ihekweaba@genesystechhub.com>
2020-12-17 23:14:07 +0100

    integrated the component with the API and linked it

--- src/api/URLs.js
+++ src/api/URLs.js
@@ -28,6 +28,7 @@ export const getDrugPricesUrl = () => `${apiUrl}/Pharmacy/GetDrugPrices`;
 export const postDrugPricesUrl = () => `${apiUrl}/Pharmacy/CreateDrugPrice`;
 export const updateDrugPricesUrl = () => `${apiUrl}/Pharmacy/UpdateDrugPrice`;
 export const deleteDrugPricesUrl = () => `${apiUrl}/Pharmacy/DeleteDrugPrice`;
+export const getAllPrescriptionsUrl = () => `${apiUrl}/Pharmacy/GetPrescriptions`;
 
 //accountant endpoints
 export const getAccountantProfileUrl = (accountantId) =>
