3b406a21697484b6bc63e0187a3c1c4a6471404b
Ogbonna <agavitalisogbonna@gmail.com>
2020-12-19 01:10:05 +0100

    retouched the admin header

--- src/Components/Header/AdminHeader.js
+++ src/Components/Header/AdminHeader.js
@@ -40,7 +40,7 @@ class AdminHeader extends React.Component {
 							</div>
 						</form>
 						<div className="app-actions">
-							{/* <div className="dropdown item">
+							<div className="dropdown item">
 								<button
 									className="no-style dropdown-toggle"
 									type="button"
@@ -50,16 +50,16 @@ class AdminHeader extends React.Component {
 									data-offset="0, 12"
 								>
 									<span className="icon icofont-notification" />{' '}
-									<span className="badge badge-danger badge-sm">5</span>
+									<span className="badge badge-danger badge-sm">0</span>
 								</button>
 								<div className="dropdown-menu dropdown-menu-right dropdown-menu-w-280">
 									<div className="menu-header">
 										<h4 className="h5 menu-title mt-0 mb-0">Notifications</h4>
-										<Link to="#" className="text-danger">
+										{/* <Link to="#" className="text-danger">
 											Clear All
-										</Link>
+										</Link> */}
 									</div>
-									<ul className="list">
+									{/* <ul className="list">
 										<li>
 											<Link to="#">
 												<span className="icon icofont-heart" />
@@ -106,13 +106,14 @@ class AdminHeader extends React.Component {
 											</Link>
 										</li>
 									</ul>
+									 */}
 									<div className="menu-footer">
 										<button className="btn btn-primary btn-block">
-											View all notifications <span className="btn-icon ml-2 icofont-tasks-alt" />
+											No notifications yet <span className="btn-icon ml-2 icofont-tasks-alt" />
 										</button>
 									</div>
 								</div>
-							</div> */}
+							</div>
 							<div className="dropdown item">
 								<button
 									className="no-style dropdown-toggle"
