a3a4b9165a8c9874d3eb1d7c60f1fdf6f15b9bce
Ogbonna <agavitalisogbonna@gmail.com>
2020-12-21 16:53:33 +0100

    fixed the redirection after register

--- src/Pages/Admin/AddPatient.js
+++ src/Pages/Admin/AddPatient.js
@@ -15,6 +15,7 @@ export default class AddPatient extends Component {
     email: "",
     healthPlan: "",
     healthPlanId: "",
+    patientId:"",
     accountId: "",
     isDisabled: true,
     success: false,
@@ -133,9 +134,9 @@ export default class AddPatient extends Component {
         }
       );
       const response = await res.json();
+      console.log(response,"Response Status")
       if (res.status === 200) {
-        this.setState({ success: true, responseID: response.response.id });
-        // this.props.history.push(""/AdminUpdatePatientProfile/"" + response.response.id)
+        this.setState({ success: true,patientId: response.patient.id });
       }
       else return;
     } catch (error) {
@@ -162,7 +163,7 @@ export default class AddPatient extends Component {
             <Success
               history={this.props.history}
               message="Well done, you successfully added a patient"
-              nextRoute={"/AdminUpdatePatientProfile/" + this.state?.responseID}
+              nextRoute={"/AdminUpdatePatientProfile/" + this.state.patientId}
             />
           ) : null}
           <div className="main-content-wrap w-75">
