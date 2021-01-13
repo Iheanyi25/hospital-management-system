dbc948856ad69e17302fd65a0150c57022de8e0a
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2021-01-13 08:08:13 +0100

    doctor viewing their patients

--- src/routes/DoctorRoutes.js
+++ src/routes/DoctorRoutes.js
@@ -17,6 +17,7 @@ import ViewServiceRequests from "../Pages/Components/ViewServiceRequests";
 import CreateServiceRequest from "../Pages/Admin/Util_Services/CreateServiceRequest";
 import ClarkingHistory from "../Pages/Components/ClarkingHistory";
 import ViewChangePassword from "../Pages/Components/ViewChangePassword";
+import MyPatients from "../Pages/Doctor/MyPatients";
 
 export default function DoctorRoutes() {
   return (
@@ -28,6 +29,11 @@ export default function DoctorRoutes() {
             exact
             path="/DoctorPatientsList"
             component={DoctorPatientsList}
+          />
+          <Route
+            exact
+            path="/myPatients"
+            component={MyPatients}
           />
           <Route
             exact
