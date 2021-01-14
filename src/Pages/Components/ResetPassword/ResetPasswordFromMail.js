9f8381067fd3c670a85d87e4c329c0ee8ecae15a
bukkysos <chukwuebukaanaynwusos@gmail.com>
2021-01-14 16:35:12 +0100

    Doone with the reset password feature

--- src/Pages/Components/ResetPassword/ResetPasswordFromMail.js
+++ src/Pages/Components/ResetPassword/ResetPasswordFromMail.js
@@ -5,7 +5,7 @@ import { fetchWrapper } from "../../../api/fetcher";
 import { fetchConfig } from "../../../api/fetchConfig";
 import { postResetPasswordFromMailUrl } from "../../../api/URLs";
 
-function ResetPasswordFromMail() {
+function ResetPasswordFromMail(props) {
   const params = new URLSearchParams(window.location.search);
   const userEmailFromLink = params.get("email");
   const userTokenFromLink = params.get("token");
@@ -17,6 +17,7 @@ function ResetPasswordFromMail() {
     serverRes: "",
     submitting: false,
     error: false,
+    errorMessage: ""
   });
 
   const {
@@ -27,166 +28,114 @@ function ResetPasswordFromMail() {
     serverRes,
     submitting,
     error,
+    errorMessage
   } = newPasswordDetails;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (newPassword !== "" && confirmPassword !== "") {
-      const resetPasswrdFromMail = postResetPasswordFromMailUrl(
-        userToken,
-        newPassword,
-        userEmail
-      );
-      const resetPasswrdFromMailConfig = fetchConfig({
-        url: resetPasswrdFromMail,
-        method: "post",
-      });
-      const res = await fetchWrapper(resetPasswrdFromMailConfig);
-
-      if (res.status === 200) {
-        console.log("Res is ", res);
-        const data = res;
-        setNewPasswordDetails({
-          ...newPasswordDetails,
-          serverRes: data.message,
+    try {
+      if (newPassword !== "" && confirmPassword !== "") {
+        const payload = {
+          authenticationToken: userToken,
+          newPassword,
+          email: userEmail
+        }
+        const resetPasswrdFromMail = postResetPasswordFromMailUrl();
+        const resetPasswrdFromMailConfig = fetchConfig({
+          url: resetPasswrdFromMail,
+          data: payload,
+          method: "post",
         });
-        // console.log('Data is ', data);
-        // localStorage.setItem("token", data.token);
-        // console.log(email, " from handleSubmit")
-        // localStorage.setItem(
-        //   "authenticatedUser",
-        //   JSON.stringify(data.authenticatedUser)
-        // );
+        const res = await fetchWrapper(resetPasswrdFromMailConfig);
+        console.log(res, 111111)
+        if (res.status === 200) {
+          console.log("Res is ", res);
+          const data = res;
+          setNewPasswordDetails({
+            ...newPasswordDetails,
+            serverRes: data.message,
+          });
+          props.history.push("/")
+          // console.log('Data is ', data);
+          // localStorage.setItem("token", data.token);
+          // console.log(email, " from handleSubmit")
+          // localStorage.setItem(
+          //   "authenticatedUser",
+          //   JSON.stringify(data.authenticatedUser)
+          // );
+        }
       }
+    } catch (error) {
+
+      console.log(error)
+      setNewPasswordDetails({
+        ...newPasswordDetails,
+        errorMessage: error.response.data.message,
+        error: true
+      })
     }
+
+
   };
   const handleNewPasswordValue = (val) => {
     setNewPasswordDetails({ ...newPasswordDetails, newPassword: val });
+    console.log(newPassword)
   };
 
   const handleConfirmPasswordValue = (val) => {
     setNewPasswordDetails({ ...newPasswordDetails, confirmPassword: val });
-  };
-
-  const setErrorStatus = () => {
-    setNewPasswordDetails({ ...newPasswordDetails, error: false });
+    console.log(confirmPassword)
   };
 
   return (
-    // <>
-    //     <main className={`${styles.background} main-content`}>
-    //         {error ? <InvalidDetails setErrorStatus={setErrorStatus()} /> : null}
-
-    //         <div className="main-content-wrap col-lg-4 col-md-6 col-sm-6 col-xs-6">
-    //             <div className="page-content">
-    //                 <div className="row justify-content-center">
-    //                     <div className="col col-md-12">
-    //                         <div className="card border-light">
-    //                             <div className="card-body bg-light pb-5">
-    //                                 <form
-    //                                     className={`${styles.form} needs-validation`}
-    //                                     noValidate
-    //                                     onSubmit={(e) => handleSubmit(e)}
-    //                                 >
-    //                                     <h4 className="text-center">Hospital Management Solution</h4>
-    //                                     <h5 className="text-center h6">Reset Password </h5>
-
-    //                                     <div className="form-group">
-    //                                         <div className="valid-feedback">Looks good!</div>
-    //                                         <div className="invalid-feedback">
-    //                                             Oops! should be numbers only.
-    //                   </div>
-    //                                         <div className="form-group">
-    //                                             <label>Password</label>
-    //                                             <input
-    //                                                 className="form-control"
-    //                                                 type="text"
-    //                                                 name="passwword"
-    //                                                 onChange={(e) => {
-    //                                                     handleNewPasswordValue(e.target.value);
-    //                                                 }}
-    //                                                 placeholder="Enter your password"
-    //                                                 required
-    //                                             />
-    //                                         </div>
-
-    //                                         <div className="form-group">
-    //                                             <label>Confirm Password</label>
-    //                                             <input
-    //                                                 className="form-control"
-    //                                                 type="text"
-    //                                                 name="confirm_password"
-    //                                                 onChange={(e) => {
-    //                                                     handleConfirmPasswordValue(e.target.value);
-    //                                                 }}
-    //                                                 placeholder="Confirm your password"
-    //                                                 required
-    //                                             />
-    //                                         </div>
-
-    //                                     </div>
-    //                                     <div className="m-auto">
-    //                                         <div className="row">
-    //                                             <button className="btn btn-primary" type="submit" disabled={submitting}>
-    //                                                 <span className="btn-icon icofont-location-arrow mr-2"></span>{" "}
-    //                                                 create new password
-    //                                                 </button>
-    //                                         </div>
-    //                                     </div>
-    //                                 </form>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </main>
-    // </>
-    <div className="auth-background d-flex justify-content-center align-items-center">
-      {error ? <InvalidDetails setErrorStatus={setErrorStatus()} /> : null}
-      <div className="card border-light">
-        <div className="card-body">
-          <form className="mb-4 p-5" onSubmit={(e) => handleSubmit(e)}>
-                <h4 className="text-center">Reset Password!</h4>
-                <div className="form-group">
-                  <label>Password</label>
-                  <input
-                    className="form-control"
-                    type="password"
-                    name="passwword"
-                    onChange={(e) => {
-                      handleNewPasswordValue(e.target.value);
-                    }}
-                    placeholder="Enter your password"
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Confirm Password</label>
-                  <input
-                    className="form-control"
-                    type="password"
-                    name="confirm_password"
-                    onChange={(e) => {
-                      handleConfirmPasswordValue(e.target.value);
-                    }}
-                    placeholder="Confirm your password"
-                    required
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-block btn-primary"
-                  type="submit"
-                  disabled={submitting}
-                >
-                  Create new password
+    <>
+      <div>
+        {error ? <InvalidDetails message={errorMessage} /> : null}
+      </div>
+      <div className="auth-background d-flex justify-content-center align-items-center">
+        <div className="card border-light">
+          <div className="card-body">
+            <form className="mb-4 p-5" onSubmit={(e) => handleSubmit(e)}>
+              <h4 className="text-center">Reset Password!</h4>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  name="passwword"
+                  onChange={(e) => {
+                    handleNewPasswordValue(e.target.value);
+                  }}
+                  placeholder="Enter your new password"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Confirm Password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  name="confirm_password"
+                  onChange={(e) => {
+                    handleConfirmPasswordValue(e.target.value);
+                  }}
+                  placeholder="Confirm your new password"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-block btn-primary"
+                type="submit"
+                disabled={submitting}
+              >
+                Create new password
                 </button>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 export { ResetPasswordFromMail };
--- src/api/urlCategories/loginUrls.js
+++ src/api/urlCategories/loginUrls.js
@@ -2,5 +2,4 @@
 export const logInUrl = () => `/Auth/Login`;
 export const postPasswordUrl = () =>`/Auth/ChangePassword`;
 export const postResetPasswordUrl = (email) =>`/Auth/SendResetPasswordMail?email=${email}`;
-export const postResetPasswordFromMailUrl = (userToken, newPassword, userEmail) =>
-  `/Auth/ResetPassword?email=${userEmail}&authenticationToken=${userToken}&newPassword=${newPassword}`;
+export const postResetPasswordFromMailUrl = () =>`/Auth/ResetPassword`;
