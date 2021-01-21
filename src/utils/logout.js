d6383e191d158106210abcef8da58ed6c166ff0b
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-01-21 15:39:45 +0100

    changed the base url

--- src/utils/logout.js
+++ src/utils/logout.js
@@ -1,5 +1,5 @@
 export const logOut = () => {
   localStorage.removeItem("authenticatedUser");
   localStorage.removeItem("token"); //to remove the token stored too
-  window.location.href = "/login"
+  window.location.href = "/"
 };
