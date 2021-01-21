d21f7df82a7bab84b8e45ca998e48383fa9c83cb
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-01-21 15:08:30 +0100

    added a generate link button and function to this component

--- src/Pages/Patient/Account.js
+++ src/Pages/Patient/Account.js
@@ -1,16 +1,23 @@
 import { observer } from "mobx-react";
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { fetchConfig } from "../../api/fetchConfig";
 import { fetchWrapper } from "../../api/fetcher";
-import { getPatientAccountBalanceUrl, getPatientAccountTransactionsUrl } from "../../api/URLs";
+import {
+  getPatientAccountBalanceUrl,
+  getPatientAccountTransactionsUrl,
+  getPatientAccountUrl,
+} from "../../api/URLs";
 import { PageLoader } from "../../Components";
+import EmptyUploadState from "../../Components/EmptyState/EmptyUploadState";
 import { UserContext } from "../../mobx/UserState";
 import formatDate from "../../utils/formatDate";
 
 const $ = window.$;
 $.Datatable = require("datatables.net");
-
+const local = "http://localhost:3000";
+const dev = " https://hms-tenece-dev.herokuapp.com";
+const live = " https://hms-tenece.herokuapp.com";
 class PatientAccount extends React.Component {
   static contextType = UserContext;
   state = {
@@ -20,33 +27,57 @@ class PatientAccount extends React.Component {
     activeAppointments: [],
     pendingAppointments: [],
     completedAppointments: [],
+    loading: true,
+    thirdPartyFundingLink: "",
   };
 
   async componentDidMount() {
     const content = this.context;
     const { user } = content;
-
     try {
-
       const getPatientAccountBalance = getPatientAccountBalanceUrl(user.id);
-      const getPatientAccountBalanceConfig = fetchConfig({ url: getPatientAccountBalance, method: "get" });
+      const getPatientAccountBalanceConfig = fetchConfig({
+        url: getPatientAccountBalance,
+        method: "get",
+      });
       const { data } = await fetchWrapper(getPatientAccountBalanceConfig);
 
-      const getPatientAccountTransactions = getPatientAccountTransactionsUrl(user.id);
-      const getPatientAccountTransactionsConfig = fetchConfig({ url: getPatientAccountTransactions, method: "get" });
-      const { data: data1 } = await fetchWrapper(getPatientAccountTransactionsConfig);
+      const getPatientAccountTransactions = getPatientAccountTransactionsUrl(
+        user.id
+      );
+      const getPatientAccountTransactionsConfig = fetchConfig({
+        url: getPatientAccountTransactions,
+        method: "get",
+      });
+      const { data: data1 } = await fetchWrapper(
+        getPatientAccountTransactionsConfig
+      );
+      const getPatientAccount = getPatientAccountUrl(user.id);
+      const getPatientAccountConfig = fetchConfig({
+        url: getPatientAccount,
+        method: "get",
+      });
+      const {
+        data: { account },
+      } = await fetchWrapper(getPatientAccountConfig);
+      this.setState({
+        thirdPartyFundingLink: `${local}/common/ThirdPartyFundAccount/${account.id}`,
+      });
+      console.log(account);
 
       console.log(data1.accountTransactions);
 
-      this.setState({
-        accountBalance: data.accountBalance,
-        accountTransactions: data1.accountTransactions,
-      }, () => this.sync());
-
+      this.setState(
+        {
+          accountBalance: data.accountBalance,
+          accountTransactions: data1.accountTransactions,
+          loading: false,
+        },
+        () => this.sync()
+      );
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
   }
 
   sync() {
@@ -54,25 +85,45 @@ class PatientAccount extends React.Component {
     this.$el.DataTable();
   }
 
+  copyToClipboard = () => {
+    const { thirdPartyFundingLink } = this.state;
+    navigator.clipboard.writeText(`${thirdPartyFundingLink}`);
+  };
 
   render() {
-    const { accountBalance, accountTransactions } = this.state;
+    const { accountBalance, accountTransactions, loading } = this.state;
     console.log(accountTransactions);
 
-    return (
+    return loading ? (
+      <PageLoader />
+    ) : (
       <>
-        <PageLoader />
-
         <main className="main-content">
           <div className="app-loader">
             <i className="icofont-spinner-alt-4 rotate" />
           </div>
           <div className="main-content-wrap">
             <header className="page-header justify-content-between d-flex align-items-center mb-2">
-              <h4 className="page-title"> Account History</h4>
-              <NavLink className="btn btn-primary" to="/PatientFundAccount">
-                Fund my account
-              </NavLink>
+              <h4 className="page-title">Account History</h4>
+              <div>
+                <Link
+                  type="button"
+                  className="btn btn-outline-primary mr-2 mb-2"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  title="Tooltip on top"
+                  to="#"
+                  onClick={this.copyToClipboard}
+                >
+                  Generate Link
+                </Link>
+                <Link
+                  className="btn btn-primary mr-2 mb-2"
+                  to="/PatientFundAccount"
+                >
+                  Fund my account
+                </Link>
+              </div>
             </header>
 
             <div className="row">
@@ -113,178 +164,69 @@ class PatientAccount extends React.Component {
             </div>
 
             <div className="page-content">
-              <div className="card mb-0">
-                <div className="card-body">
-                  <div>
-                    <div className="tab-content" id="pills-tabContent">
-                      <div
-                        className="tab-pane fade show active"
-                        id="pills-active"
-                        role="tabpanel"
-                        aria-labelledby="pills-active-tab"
-                      >
-                        <div className="table-responsive">
-                          <table
-                            ref={(el) => (this.el = el)}
-                            className="table table-striped"
-                            data-paging="true"
-                            data-info="true"
-                          >
-                            <thead>
-                              <tr>
-                                <th>Amount</th>
-                                <th>Transaction Type</th>
-                                <th>Paid By</th>
-                                <th>Medium Of Payment</th>
-                                <th>Date</th>
-                                <th>Account Balance</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              {accountTransactions
-                                ? accountTransactions.map(
-                                  (transaction, index) => (
-                                    <tr key={index}>
-                                      <td>{transaction.amount}</td>
-                                      <td>
-                                        <td>{transaction.transactionType}</td>
-                                      </td>
-                                      <td>
-                                        <td>{transaction.paidBy}</td>
-                                      </td>
-                                      <td>
-                                        <td>{transaction.description}</td>
-                                      </td>
-                                      <td>
-                                        <td>
-                                          {formatDate(
-                                            transaction.trasactionDate
-                                          )}
-                                        </td>
-                                      </td>
-                                      <td>
-                                        <td>{transaction.amount}</td>
-                                      </td>
-                                    </tr>
-                                  )
-                                )
-                                : null}
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-accepted"
-                        role="tabpanel"
-                        aria-labelledby="pills-accepted-tab"
-                      >
-                        <div className="table-responsive">
-                          <table
-                            className="table data-table"
-                            data-columns='[
-                                                        { "data": "photo" },
-                                                        { "data": "name" },
-                                                        { "data": "email" },
-                                                        { "data": "phone" },
-                                                        { "data": "date-of-birth" },
-                                                        { "data": "address" },
-                                                        { "data": "actions" }
-                                                    ]'
-                            data-paging="true"
-                            data-info="true"
-                          >
-                            <thead>
-                              <tr className="bg-primary text-white">
-                                <th>Photo</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Date Of Birth</th>
-                                <th>Address</th>
-                                <th>Actions</th>
-                              </tr>
-                            </thead>
-                            <tbody></tbody>
-                          </table>
-                        </div>
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-completed"
-                        role="tabpanel"
-                        aria-labelledby="pills-completed-tab"
-                      >
-                        <div className="table-responsive">
-                          <table
-                            className="table data-table"
-                            data-columns='[
-                                                        { "data": "photo" },
-                                                        { "data": "name" },
-                                                        { "data": "email" },
-                                                        { "data": "phone" },
-                                                        { "data": "date-of-birth" },
-                                                        { "data": "address" },
-                                                        { "data": "actions" }
-                                                    ]'
-                            data-paging="true"
-                            data-info="true"
-                          >
-                            <thead>
-                              <tr className="bg-primary text-white">
-                                <th>Photo</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Date Of Birth</th>
-                                <th>Address</th>
-                                <th>Actions</th>
-                              </tr>
-                            </thead>
-                            <tbody></tbody>
-                          </table>
-                        </div>
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-pending"
-                        role="tabpanel"
-                        aria-labelledby="pills-pending-tab"
-                      >
-                        <div className="table-responsive">
-                          <table
-                            className="table data-table"
-                            data-columns='[
-                                                                    { "data": "photo" },
-                                                                    { "data": "name" },
-                                                                    { "data": "email" },
-                                                                    { "data": "phone" },
-                                                                    { "data": "date-of-birth" },
-                                                                    { "data": "address" },
-                                                                    { "data": "actions" }
-                                                                ]'
-                            data-paging="true"
-                            data-info="true"
-                          >
-                            <thead>
-                              <tr className="bg-primary text-white">
-                                <th>Photo</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Date Of Birth</th>
-                                <th>Address</th>
-                                <th>Actions</th>
-                              </tr>
-                            </thead>
-                            <tbody></tbody>
-                          </table>
+              {accountTransactions.length === 0 ? (
+                <div
+                  className="d-flex justify-content-center"
+                  style={{ height: "200px", marginTop: "100px" }}
+                >
+                  <EmptyUploadState message="You haven’t made any payments" />
+                </div>
+              ) : (
+                <div className="card mb-0">
+                  <div className="card-body">
+                    <div>
+                      <div className="tab-content" id="pills-tabContent">
+                        <div
+                          className="tab-pane fade show active"
+                          id="pills-active"
+                          role="tabpanel"
+                          aria-labelledby="pills-active-tab"
+                        >
+                          <div className="table-responsive">
+                            <table
+                              ref={(el) => (this.el = el)}
+                              className="table table-striped"
+                              data-paging="true"
+                              data-info="true"
+                            >
+                              <thead>
+                                <tr>
+                                  <th>Amount</th>
+                                  <th>Transaction Type</th>
+                                  <th>Paid By</th>
+                                  <th>Medium Of Payment</th>
+                                  <th>Date</th>
+                                  <th>Account Balance</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {accountTransactions
+                                  ? accountTransactions.map(
+                                      (transaction, index) => (
+                                        <tr key={index}>
+                                          <td>{transaction.amount}</td>
+                                          <td>{transaction.transactionType}</td>
+                                          <td>{transaction.paidBy}</td>
+                                          <td>{transaction.description}</td>
+                                          <td>
+                                            {formatDate(
+                                              transaction.trasactionDate
+                                            )}
+                                          </td>
+                                          <td>{transaction.amount}</td>
+                                        </tr>
+                                      )
+                                    )
+                                  : null}
+                              </tbody>
+                            </table>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </main>
