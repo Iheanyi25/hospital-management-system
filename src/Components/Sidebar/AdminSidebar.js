fca49dfd569aeee0b8b048d0d9c9a855bab0bd27
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-01-27 19:01:47 +0100

    added the link to the report page

--- src/Components/Sidebar/AdminSidebar.js
+++ src/Components/Sidebar/AdminSidebar.js
@@ -9,17 +9,6 @@ class AdminSidebar extends React.Component {
       endpoint: process.env.REACT_APP_API_URL,
     };
   }
-  componentWillMount() {
-    // $('.navbar-toggle').click(function () {
-    //   $('.app-navbar.vertical, .app-navbar.horizontal-vertical').toggleClass('opened');
-    //   $('.content-overlay').toggleClass('show');
-    // });
-    // $('.content-overlay').click(function () {
-    //   $('.app-navbar.vertical, .app-navbar.horizontal-vertical').removeClass('opened');
-    //   $(this).removeClass('show');
-    // });
-    // console.log($);
-  }
 
   render() {
     const { setUserType } = this.props;
@@ -388,13 +377,14 @@ class AdminSidebar extends React.Component {
                   </li>
 
                   <li className="menu-item">
+                    <span className="group-title">Reports</span>
+                  </li>
+                  <li className="menu-item">
                     <Link className="item-link" to="/AdminRegistrationReport">
                       <span className="link-icon icofont-paper" />{" "}
                       <span className="link-text">Registration report</span>
                     </Link>
                   </li>
-
-
                   <li className="menu-item">
                     <span className="group-title">PHARMACY</span>
                   </li>
