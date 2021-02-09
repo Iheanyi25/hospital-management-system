5a5da2e7e3ce2cc740bab105ad9a989469d22a56
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-02-09 11:24:01 +0100

    added nullish coallision

--- src/Pages/Components/PatientHistory/PatientProfileInfo.js
+++ src/Pages/Components/PatientHistory/PatientProfileInfo.js
@@ -3,87 +3,88 @@ import PatientImg from "../../../assets/img/user.png";
 import { FlexBetweenContainer } from "../../../Components/reusable-css-in-js-components/Flex";
 
 export const PatientBioDetails = ({ profile }) => {
-    return (
-      <FlexBetweenContainer width="22rem">
+  return (
+    <FlexBetweenContainer width="22rem">
+      <div>
+        <img src={PatientImg} alt="patient profile" />
+      </div>
+      <div>
+        <h3
+          className="font-weight-bold"
+          style={{ textTransform: "capitalize" }}
+        >{`${profile.firstname} ${profile.lastname}`}</h3>
+        <div></div>
+      </div>
+    </FlexBetweenContainer>
+  );
+};
+
+export const PatientHealthDetails = ({ profile }) => {
+  return (
+    <div>
+      <h5 className="font-weight-bold">Health Details</h5>
+      <FlexBetweenContainer width="30rem">
         <div>
-          <img src={PatientImg} alt="patient profile" />
+          <h6>Blood Group</h6>
+          <div>{profile?.bloodGroup ?? "N/A"}</div>
         </div>
         <div>
-          <h3 className="font-weight-bold">{`${profile.firstname} ${profile.lastname}`}</h3>
-          <div></div>
+          <h6>Genotype</h6>
+          <div>{profile?.genotype ?? "N/A"}</div>
+        </div>
+        <div>
+          <h6>Gender</h6>
+          <div>{profile?.gender ?? "N/A"}</div>
+        </div>
+        <div>
+          <h6>Diabetic</h6>
+          <div>{profile?.diabetic ? "True" : "False"}</div>
         </div>
       </FlexBetweenContainer>
-    );
-  };
-  
- export const PatientHealthDetails = ({ profile }) => {
-    return (
-      <div>
-        <h5 className="font-weight-bold">Health Details</h5>
-        <FlexBetweenContainer width="30rem">
-          <div>
-            <h6>Blood Group</h6>
-            <div>{profile?.bloodGroup}</div>
-          </div>
-          <div>
-            <h6>Genotype</h6>
-            <div>{profile?.genotype}</div>
-          </div>
-          <div>
-            <h6>Gender</h6>
-            <div>{profile?.gender}</div>
-          </div>
-          <div>
-            <h6>Diabetic</h6>
-            <div>{profile?.diabetic ? "True" : "False"}</div>
-          </div>
-        </FlexBetweenContainer>
-  
-        <FlexBetweenContainer width="38rem">
-          <div>
-            <h6>Allergies</h6>
-            <div>{profile?.allergies}</div>
-          </div>
-          <div>
-            <h6>Disabilities</h6>
-            <div>{profile?.disabilities}</div>
-          </div>
-        </FlexBetweenContainer>
-      </div>
-    );
-  };
-  
 
-  export const PatientContactDetails = ({ profile }) => {
-    return (
-      <div>
-        <h5 className="font-weight-bold">Contact Information</h5>
-        <FlexBetweenContainer width="18rem">
-          <div>
-            <h6>Mobile</h6>
-            <div>{profile?.phoneNumber}</div>
-          </div>
-          <div>
-            <h6>Email</h6>
-            <div>{profile?.email}</div>
-          </div>
-        </FlexBetweenContainer>
-  
-        <FlexBetweenContainer width="42rem">
-          <div>
-            <h6>Address</h6>
-            <div>{profile?.address}</div>
-          </div>
-          <div>
-            <h6>Country</h6>
-            <div>{profile?.country}</div>
-          </div>
-          <div>
-            <h6>State of Origin</h6>
-            <div>{profile?.state}</div>
-          </div>
-        </FlexBetweenContainer>
-      </div>
-    );
-  };
-  
+      <FlexBetweenContainer width="38rem">
+        <div>
+          <h6>Allergies</h6>
+          <div>{profile?.allergies ?? "N/A"}</div>
+        </div>
+        <div>
+          <h6>Disabilities</h6>
+          <div>{profile?.disabilities ?? "N/A"}</div>
+        </div>
+      </FlexBetweenContainer>
+    </div>
+  );
+};
+
+export const PatientContactDetails = ({ profile }) => {
+  return (
+    <div>
+      <h5 className="font-weight-bold">Contact Information</h5>
+      <FlexBetweenContainer width="18rem">
+        <div>
+          <h6>Mobile</h6>
+          <div>{profile?.phoneNumber ?? "N/A"}</div>
+        </div>
+        <div>
+          <h6>Email</h6>
+          <div>{profile?.email ?? "N/A"}</div>
+        </div>
+      </FlexBetweenContainer>
+
+      <FlexBetweenContainer width="42rem">
+        <div>
+          <h6>Address</h6>
+          <div>{profile?.address ?? "N/A"}</div>
+        </div>
+        <div>
+          <h6>Country</h6>
+          <div>{profile?.country ?? "N/A"}</div>
+        </div>
+        <div>
+          <h6>State of Origin</h6>
+          <div>{profile?.state ?? "N/A"}</div>
+        </div>
+      </FlexBetweenContainer>
+    </div>
+  );
+};
