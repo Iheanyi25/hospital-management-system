f3f6c774b688000d690eab11a8ce8abe48d56f4a
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-02-15 09:09:19 +0100

    broke the appointment summary down into a component of its own

--- src/Pages/Doctor/Appointments.js
+++ src/Pages/Doctor/Appointments.js
@@ -13,6 +13,7 @@ import { PageLoader } from "../../Components";
 import { UserContext } from "../../mobx/UserState";
 import formatDate from "../../utils/formatDate";
 import formatTime from "../../utils/formatTime";
+import { AppointmentSummary } from "./appointment-components";
 
 const $ = window.$;
 $.Datatable = require("datatables.net");
@@ -180,61 +181,11 @@ class Appointments extends React.Component {
             <i className="icofont-spinner-alt-4 rotate" />
           </div>
           <div className="main-content-wrap">
-            <div className="row">
-              <div className="col col-12 col-md-6 col-xl-4">
-                <div className="card animated fadeInUp delay-02s bg-light">
-                  <div className="card-body">
-                    <div className="row align-items-center">
-                      <div className="col col-5">
-                        <div className="icon p-0 fs-48 text-primary opacity-50 icofont-wheelchair"></div>
-                      </div>
-                      <div className="col col-7">
-                        <h6 className="mt-0 mb-1">Pending Appointments</h6>
-                        <div className="count text-primary fs-20">
-                          {pendingAppointmentsCount}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col col-12 col-md-6 col-xl-4">
-                <div className="card animated fadeInUp delay-03s bg-light">
-                  <div className="card-body">
-                    <div className="row align-items-center">
-                      <div className="col col-5">
-                        <div className="icon p-0 fs-48 text-primary opacity-50 icofont-blood" />
-                      </div>
-                      <div className="col col-7">
-                        <h6 className="mt-0 mb-1">Accepted Appointments</h6>
-                        <div className="count text-primary fs-20">
-                          {acceptedAppointmentsCount}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col col-12 col-md-6 col-xl-4">
-                <div className="card animated fadeInUp delay-04s bg-light">
-                  <div className="card-body">
-                    <div className="row align-items-center">
-                      <div className="col col-5">
-                        <div className="icon p-0 fs-48 text-primary opacity-50 icofont-list"></div>
-                      </div>
-                      <div className="col col-7">
-                        <h6 className="mt-0 mb-1 text-nowrap">
-                          Rejected Appointments
-                        </h6>
-                        <div className="count text-primary fs-20">
-                          {rejectedAppointmentsCount}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <AppointmentSummary
+              pendingAppointmentsCount={pendingAppointmentsCount}
+              acceptedAppointmentsCount={acceptedAppointmentsCount}
+              rejectedAppointmentsCount={rejectedAppointmentsCount}
+            />
 
             <header className="page-header">
               <h4 className="page-title"> Appointments List</h4>
