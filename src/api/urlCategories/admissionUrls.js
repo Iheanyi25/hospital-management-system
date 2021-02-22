a1d61f98b64a5ea9d773d93a097954b1ece762e1
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-02-22 12:34:05 +0100

    added the get admissions URL

--- src/api/urlCategories/admissionUrls.js
+++ src/api/urlCategories/admissionUrls.js
@@ -1,6 +1,8 @@
 // Admin - Manage Admission
+export const getAdmissionsUrl = (pageNumber, pageSize) =>
+  `/Admission/GetAdmissions?PageNumber=${pageNumber}&PageSize=${pageSize}`;
 export const getAdmissionsWithoutBedUrl = (pageNumber, pageSize) =>
   `/Admission/GetAdmissionsWithoutBed?PageNumber=${pageNumber}&PageSize=${pageSize}`;
-  
+
 // Admission - Manage Wards
 export const createWardUrl = () => `/Admission/Ward/CreateWard`;
