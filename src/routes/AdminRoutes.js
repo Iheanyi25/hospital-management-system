c82f5aa98d52bfe87a9a238d171eae171009a258
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-02-22 16:45:18 +0100

    Addded manage beds component to the route

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -71,6 +71,7 @@ import ViewPatientHealthHistory from "../Pages/Admin/ViewPatientHealthHistory";
 import CreateWard from "../Components/Admissions/Wards/CreateWard";
 import EditWard from "../Components/Admissions/Wards/EditWard";
 import ManageWards from "../Components/Admissions/Wards/ManageWards";
+import ManageBeds from "../Components/Admissions/Wards/ManageBeds";
 import ManageAdmissions from "../Components/Admissions/ManageAdmissions";
 import ReferredPatients from "../Components/Admissions/ReferredPatients";
 
@@ -172,6 +173,7 @@ export default function AdminRoutes() {
           />
           <Route exact path="/AdminCreateWard" component={CreateWard} />
           <Route exact path="/AdminEditWard/:id" component={EditWard} />
+          <Route exact path="/AdminManageBeds/:id" component={ManageBeds} />
           <Route exact path="/DoctorProfile/:id" component={DoctorsProfile} />
           <Route exact path="/AdminManageWards" component={ManageWards} />
           <Route
