a6d9913a10c54aa4d850bc7e504428442929ba32
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-06 09:55:38 +0100

    added the route for nurse

--- src/ApplicationRoute.js
+++ src/ApplicationRoute.js
@@ -10,6 +10,7 @@ import AccountantRoutes from "./routes/AccountantRoutes";
 import { UserContext } from "./mobx/UserState";
 import { observer } from "mobx-react";
 import { CommonRoute } from "./routes/CommonRoutes";
+import NurseRoutes from "./routes/NurseRoutes";
 
 const AppRouter = observer(() => {
   const { loadUser, user, isLoadingUser } = useContext(UserContext);
@@ -48,6 +49,8 @@ const AppRouter = observer(() => {
           return <LabRoutes />;
         case "accountant":
           return <AccountantRoutes />;
+        case "nurse":
+          return <NurseRoutes />;
         default:
           localStorage.clear();
           window.location.reload();
