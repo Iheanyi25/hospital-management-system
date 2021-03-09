0224bad6b4c897c43ebe8f3ebd470f21baf352a6
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-09 11:18:19 +0100

    fixed the bug on the component

--- src/Pages/Admin/Util_Services/ServiceRequestContents.js
+++ src/Pages/Admin/Util_Services/ServiceRequestContents.js
@@ -45,13 +45,12 @@ const ServiceRequestContents = ({ match, location }) => {
       if (userType === "Admin" || userType === "Lab") {
         return {
           "#": ++index,
-          "Patient's Name": `${request?.serviceInvoice.patient.firstName} ${request?.serviceInvoice.patient.lastName}`,
-          "Service Category": request?.service?.serviceCategory?.name,
           "Service Name": request?.serviceName,
+          "Service Category": request?.service?.serviceCategory?.name,
           Amount: formatAmount(request?.cost) ?? "",
           Status: (
             <>
-              {request?.paymentStatus === "False" ? (
+              {request?.status === "False" ? (
                 <>
                   <img src={notpaid} alt="not paid" /> Not paid
                 </>
@@ -78,7 +77,7 @@ const ServiceRequestContents = ({ match, location }) => {
           Amount: formatAmount(request?.amount) ?? "",
           Status: (
             <>
-              {request?.paymentStatus === "False" ? (
+              {request?.status === "NOT PAID" ? (
                 <>
                   <img src={notpaid} alt="not paid" /> Not paid
                 </>
