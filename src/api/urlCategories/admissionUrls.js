f71428538474061b2fc60c04b5872a38fb731c00
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-11 13:20:11 +0100

    added pagination element to URL

--- src/api/urlCategories/admissionUrls.js
+++ src/api/urlCategories/admissionUrls.js
@@ -12,8 +12,8 @@ export const getDrugsInAnAdmissionInvoiceUrl = (invoiceId) =>
   `/Admission/GetDrugsInAnInvoice?invoiceId=${invoiceId}`;
 
 // Admission - Manage Admission Invoices
-export const getAdmissionTransactionsUrl = (admssionId) =>
-  `/Admission/GetAdmissionTransactions?AdmissionId=${admssionId}`;
+export const getAdmissionTransactionsUrl = (admssionId, pageNumber, pageSize) =>
+  `/Admission/GetAdmissionTransactions?AdmissionId=${admssionId}&PageNumber=${pageNumber}&PageSize=${pageSize}`;
 export const getAdmissionInvoiceUrl = (admssionId) =>
   `/Admission/GetAdmissionInvoice?AdmissionId=${admssionId}`;
 export const postPayForAdmissionUrl = () => `/Admission/PayForAdmission`;
