057ed96226f627e19b8311a79c5361db9cb2bcba
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-11 13:20:36 +0100

    integrated the view all nurses endpoint

--- src/api/URLs.js
+++ src/api/URLs.js
@@ -4,6 +4,7 @@ export * from './urlCategories/adminUrls.js';
 export * from './urlCategories/admissionUrls';
 export * from './urlCategories/accountantUrls.js';
 export * from './urlCategories/doctorUrls.js';
+export * from './urlCategories/nurseUrls.js';
 export * from './urlCategories/labUrls.js';
 export * from './urlCategories/pharmacyUrls.js';
 export * from './urlCategories/patientUrls.js';
