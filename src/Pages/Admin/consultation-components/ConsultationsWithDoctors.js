cb8fedc79fde754ebeaa8458ac350775d5497a10
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-12 12:54:04 +0100

    fixed the reassign bug

--- src/Pages/Admin/consultation-components/ConsultationsWithDoctors.js
+++ src/Pages/Admin/consultation-components/ConsultationsWithDoctors.js
@@ -16,7 +16,6 @@ import { notification } from "../../../utils/notification";
 const ConsultationsWithDoctors = () => {
   const [pageNumber, setPageNumber] = useState(1);
   const [pageSize, setPageSize] = useState(50);
-  const [consultationId, setConsultationId] = useState();
   const patientConsultationsWithDoctorsCount = getPatientConsultationsWithDoctorsUrl(
     pageNumber,
     pageSize
@@ -68,8 +67,8 @@ const ConsultationsWithDoctors = () => {
         Actions: (
           <ConsultationsWithDoctorsActionTable
             consultation={consultation}
-            setConsultationId={setConsultationId}
             deleteConsultation={deleteConsultation}
+            mutate={mutate}
           />
         ),
       };
@@ -88,76 +87,80 @@ const ConsultationsWithDoctors = () => {
         pageSize={pageSize}
         setPageSize={setPageSize}
       />
-      <ReAssign
-        idType="consultationId"
-        route={"ReassignAppointment"}
-        reRun={mutate}
-        id={consultationId}
-      />
     </div>
   );
 };
 
 export const ConsultationsWithDoctorsActionTable = observer(
-  ({ consultation, setConsultationId, deleteConsultation }) => {
+  ({ consultation, deleteConsultation, mutate }) => {
     const {
       user: { userType },
     } = useContext(UserContext);
     return (
-      <ActionButton>
-        <Link
-          title="Go For Clerking"
-          to={{
-            pathname: `/DoctorClarking`,
-            state: {
-              id: consultation.id,
-              type: "consultation",
-              patient: consultation.patient,
-            },
-          }}
-          key={`/DoctorClarking`}
-          className="btn btn-sm btn-block"
-        >
-          <span className="btn-icon icofont-stethoscope-alt mr-2" />
-          Go For Clerking
-        </Link>
-        <Link
-          title="Go For Pre-consultation"
-          to={
-            userType === "Nurse"
-              ? `/NursePreConsultation/${consultation.patient.id}`
-              : `/AdminPreConsultation/${consultation.patient.id}`
-          }
-          className="btn btn-sm btn-block"
-        >
-          <span className="btn-icon icofont-stethoscope-alt mr-2" />
-          Go For Pre-consultation
-        </Link>
-        <Link
-          title="Clerking History"
-          to={{ pathname: `/ViewClarkingHistory`, state: consultation.patient }}
-          className="btn btn-sm btn-block"
-        >
-          <span className="btn-icon icofont-stethoscope-alt mr-2" />
-          Clerking History
-        </Link>
-        <button
-          className="btn btn-sm btn-block"
-          data-toggle="modal"
-          onClick={() => setConsultationId(consultation.id)}
-          data-target={`#reassign-patient-${consultation.id}`}
-        >
-          <span className="mr-3 btn-icon icofont-stethoscope-alt" />
-          Assign to Doctor
-        </button>
-        <button
-          className="btn btn-sm btn-block"
-          onClick={() => deleteConsultation(consultation.id)}
-        >
-          <span className="mr-3 btn-icon icofont-delete-alt" />
-          Delete Consultation
-        </button>
-      </ActionButton>
+      <div>
+        <ActionButton>
+          <Link
+            title="Go For Clerking"
+            to={{
+              pathname: `/DoctorClarking`,
+              state: {
+                id: consultation.id,
+                type: "consultation",
+                patient: consultation.patient,
+              },
+            }}
+            key={`/DoctorClarking`}
+            className="btn btn-sm btn-block"
+          >
+            <span className="btn-icon icofont-stethoscope-alt mr-2" />
+            Go For Clerking
+          </Link>
+          <Link
+            title="Go For Pre-consultation"
+            to={
+              userType === "Nurse"
+                ? `/NursePreConsultation/${consultation.patient.id}`
+                : `/AdminPreConsultation/${consultation.patient.id}`
+            }
+            className="btn btn-sm btn-block"
+          >
+            <span className="btn-icon icofont-stethoscope-alt mr-2" />
+            Go For Pre-consultation
+          </Link>
+          <Link
+            title="Clerking History"
+            to={{
+              pathname: `/ViewClarkingHistory`,
+              state: consultation.patient,
+            }}
+            className="btn btn-sm btn-block"
+          >
+            <span className="btn-icon icofont-stethoscope-alt mr-2" />
+            Clerking History
+          </Link>
+          <button
+            className="btn btn-sm btn-block"
+            data-toggle="modal"
+            data-target={`#reassign-patient-${consultation.id}`}
+          >
+            <span className="mr-3 btn-icon icofont-stethoscope-alt" />
+            Assign to Doctor
+          </button>
+          <button
+            className="btn btn-sm btn-block"
+            onClick={() => deleteConsultation(consultation.id)}
+          >
+            <span className="mr-3 btn-icon icofont-delete-alt" />
+            Delete Consultation
+          </button>
+        </ActionButton>
+        <ReAssign
+          idType="consultationId"
+          route={"ReassignAppointment"}
+          reRun={mutate}
+          id={consultation.id}
+        />
+      </div>
     );
   }
 );
