0ae8d2a9c9cd6869f2f13b6c66967b2988bc2570
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-17 13:40:46 +0100

    added the API for creating a note

--- src/api/urlCategories/admissionUrls.js
+++ src/api/urlCategories/admissionUrls.js
@@ -20,6 +20,11 @@ export const postPayForAdmissionUrl = () => `/Admission/PayForAdmission`;
 export const postPayForAdmissionWithAccountUrl = () =>
   `/Admission/PayForAdmissionWithAccount`;
 
+// Admission - Manage Admission Notes
+export const getAdmissionsDoctorsNotesUrl = (AdmissionId) =>
+  `Admission/GetAdmissionNotesForAdmission?AdmissionId=${AdmissionId}`;
+export const createAdmissionsNoteUrl = () => `Admission/CreateAdmissionNote`;
+
 // Admission - Manage Admission Service Requests
 export const postAdmissionsRequestServiceUrl = () =>
   `/Admission/RequestServices`;
@@ -42,9 +47,6 @@ export const deleteWardUrl = () => `/Admission/Ward/DeleteWard`;
 export const getBedsInAWardUrl = (wardId, pageNumber, pageSize) =>
   `/Admission/Ward/GetBedsInAWard?WardId=${wardId}&PageNumber=${pageNumber}&PageSize=${pageSize}`;
 
-// Admission - Manage Wards
-export const getAdmissionsDoctorsNotesUrl = (AdmissionId) =>
-  `Admission/GetAdmissionNotesForAdmission?AdmissionId=${AdmissionId}`;
 // Admission - Manage Prescriptions
 export const getPrescriptionForAdmssionUrl = (id) =>
   `/Admission/GetPrescription?PrescriptionId=${id}`;
