313081569785ccfff70203d815846a28531d2743
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-17 13:41:54 +0100

    added the modal for updating notes and empty state

--- src/Components/Admissions/DoctorsNotes.js
+++ src/Components/Admissions/DoctorsNotes.js
@@ -6,9 +6,7 @@ import { getAdmissionsDoctorsNotesUrl } from "../../api/URLs";
 import formatDate from "../../utils/formatDate";
 import { PageLoader } from "../Loader";
 import EmptyUploadState from "../../Components/EmptyState/EmptyUploadState";
-
-let $ = window.$;
-$.DataTables = require("datatables.net");
+import UpdateDoctorsNotes from "../Modals/UpdateDoctorsNotes";
 
 const DoctorsNotes = ({ admissionId }) => {
   const getAdmissionsDoctorsNotes = getAdmissionsDoctorsNotesUrl(admissionId);
@@ -16,74 +14,97 @@ const DoctorsNotes = ({ admissionId }) => {
     url: getAdmissionsDoctorsNotes,
     method: "get",
   });
-  const { data } = useRequest(getAdmissionsDoctorsNotesConfig, {
+  const { data, mutate } = useRequest(getAdmissionsDoctorsNotesConfig, {
     revalidateOnFocus: false,
   });
   return (
     <>
-      <div className="card-body">
-        <div id="accordion" className="mb-3">
-          {!data ? (
-            <PageLoader />
-          ) : data?.admissionNotes?.length === 0 ? (
-            <EmptyUploadState message="No Doctors Notes" />
-          ) : (
-            data?.admissionNotes.map((doctorsnote, index) => (
-              <div className="card mb-0">
-                <div className="card-header" id={`heading${1}`}>
-                  <h5 className="mb-0">
-                    <button
-                      className="btn btn-outline-primary btn-block"
-                      data-toggle="collapse"
-                      data-target={`#collapseDrNote${index + 1}`}
-                      aria-expanded="true"
-                      aria-controls={`collapse${index + 1}`}
-                    >
-                      Doctors Notes
-                      {`Captured on ${
-                        formatDate(doctorsnote.dateGenerated) ?? ""
-                      }`}
-                    </button>
-                  </h5>
+      <div className="row justify-content-center w-50 mx-auto mt-5">
+        <div className="col-md-12">
+          <div className="card border-light">
+            <div className="card-body">
+              {data?.admissionNotes.length > 0 ? (
+                <div className="d-flex justify-content-between align-item-between">
+                  <h5 className="m-0">Doctor's Notes</h5>
+                  <button
+                    className="btn btn-primary"
+                    data-toggle="modal"
+                    data-target="#doctors-note"
+                  >
+                    Update
+                  </button>
                 </div>
-                <div
-                  id={`collapseDrNote${index + 1}`}
-                  className="collapse"
-                  aria-labelledby="headingOne"
-                  data-parent="#accordion"
-                >
-                  <div className="d-flex mt-3 mb-3">
-                    <img
-                      src={user}
-                      style={{ height: "64px", width: "64px" }}
-                      className="rounded-circle mr-3"
-                      alt="user"
-                    />
-                    <div>
-                      <h5 className="mb-2 mt-1 font-weight-bold">
-                        <u>
-                          {`Dr. ${doctorsnote.doctor.firstName}  ${doctorsnote.doctor.lastName}`}
-                        </u>
-                      </h5>
-                      <p className="mb-2">
-                        {" "}
-                        {`Clerked patient on ${
-                          formatDate(doctorsnote.dateGenerated) ?? ""
-                        }`}
-                      </p>
+              ) : null}
+
+              <div id="accordion" className="mb-3">
+                {!data ? (
+                  <PageLoader />
+                ) : data?.admissionNotes?.length === 0 ? (
+                  <EmptyUploadState
+                    message="No Doctors Notes"
+                    target="#doctors-note"
+                    targetDescription="Update notes"
+                  />
+                ) : (
+                  data?.admissionNotes.map((doctorsnote, index) => (
+                    <div className="card mb-0">
+                      <div className="card-header" id={`heading${1}`}>
+                        <h5 className="mb-0">
+                          <button
+                            className="btn btn-outline-primary btn-block"
+                            data-toggle="collapse"
+                            data-target={`#collapseDrNote${index + 1}`}
+                            aria-expanded="true"
+                            aria-controls={`collapse${index + 1}`}
+                          >
+                            {`Captured on ${
+                              formatDate(doctorsnote.dateGenerated) ?? ""
+                            }`}
+                          </button>
+                        </h5>
+                      </div>
+                      <div
+                        id={`collapseDrNote${index + 1}`}
+                        className="collapse"
+                        aria-labelledby="headingOne"
+                        data-parent="#accordion"
+                      >
+                        <div className="d-flex mt-3 mb-3">
+                          <img
+                            src={user}
+                            style={{ height: "64px", width: "64px" }}
+                            className="rounded-circle mr-3"
+                            alt="user"
+                          />
+                          <div>
+                            <h5 className="mb-2 mt-1 font-weight-bold">
+                              <u>
+                                {`Dr. ${doctorsnote.doctor.firstName}  ${doctorsnote.doctor.lastName}`}
+                              </u>
+                            </h5>
+                            <p className="mb-2">
+                              {" "}
+                              {`Clerked patient on ${
+                                formatDate(doctorsnote.dateGenerated) ?? ""
+                              }`}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="card-body w-50">
+                          <div className="pl-5">
+                            <p className="mb-0">{doctorsnote.note}</p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="card-body w-50">
-                    <div className="pl-5">
-                      <p className="mb-0">{doctorsnote.note}</p>
-                    </div>
-                  </div>
-                </div>
+                  )) ?? null
+                )}
               </div>
-            )) ?? null
-          )}
+            </div>
+          </div>
         </div>
       </div>
+      <UpdateDoctorsNotes admissionId={admissionId} mutate={mutate} />
     </>
   );
 };
