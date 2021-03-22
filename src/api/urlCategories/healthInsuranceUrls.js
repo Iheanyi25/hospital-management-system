c3f8c147dbfe5aecbfc497ce7aa8e5c33689407a
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-22 14:33:40 +0100

    added the endpoint for getting plans

--- src/api/urlCategories/healthInsuranceUrls.js
+++ src/api/urlCategories/healthInsuranceUrls.js
@@ -1,4 +1,7 @@
 const baseName = "HealthInsurance";
+
 // Health Insurance - Manage NHIS Health Plans
+export const getNHISHealthPlansUrl = (pageNumber, pageSize) =>
+  `/${baseName}/GetNHISHealthPlans?PageNumber=${pageNumber}&PageSize=${pageSize}`;
 export const createNHISHealthPlanUrl = () =>
   `/${baseName}/CreateNHISHealthPlan`;
