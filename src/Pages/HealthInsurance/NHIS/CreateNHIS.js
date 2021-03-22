487b5984ef652c69d9012c9364a5289dc7604728
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-22 13:58:09 +0100

    integrated this component with the endpoint

--- src/Pages/HealthInsurance/NHIS/CreateNHIS.js
+++ src/Pages/HealthInsurance/NHIS/CreateNHIS.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import Select from "react-select";
+import { fetchConfig } from "../../../api/fetchConfig";
+import { fetchWrapper, useRequest } from "../../../api/fetcher";
+import {
+  getAllHealthPlansUrl,
+  createNHISHealthPlanUrl,
+} from "../../../api/URLs";
 import { PageLoader } from "../../../Components";
+import { notification } from "../../../utils/notification";
+import { isNotEmptyString } from "../../../utils/validationUtils";
 
 export default function CreateInsurance() {
+  const [payload, setPayload] = useState({
+    name: "",
+    percentage: "",
+    amount: "",
+    description: "",
+    requireAuthorizationCode: false,
+    healthPlan: null,
+  });
+  const [emptyField, setEmptyField] = useState(true);
+  useEffect(() => {
+    const { name, percentage, amount, healthPlan } = payload;
+    if (
+      isNotEmptyString(name) &&
+      isNotEmptyString(percentage) &&
+      isNotEmptyString(amount) &&
+      isNotEmptyString(healthPlan)
+    ) {
+      setEmptyField(false);
+    }
+  }, [payload]);
+  const history = useHistory();
+  const getAllHealthPlans = getAllHealthPlansUrl(1, 200);
+  const getAllHealthPlansConfig = fetchConfig({
+    url: getAllHealthPlans,
+    method: "get",
+  });
+  const { data, error } = useRequest(getAllHealthPlansConfig, {
+    revalidateOnFocus: false,
+  });
+  const options = [];
+
+  if (data?.healthPlans.length > 0) {
+    data.healthPlans.forEach(({ id, name }) => {
+      options.push({ value: id, label: name });
+    });
+  }
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      setPayload({ ...payload, requireAuthorizationCode: e.target.checked });
+    } else {
+      setPayload({
+        ...payload,
+        [e.target.name]: e.target.value,
+      });
+    }
+    console.log(payload);
+  };
+  const handleSelect = (healthPlan) => {
+    console.log(healthPlan);
+    setPayload({ ...payload, healthPlan });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const {
+      percentage,
+      amount,
+      description,
+      requireAuthorizationCode,
+      healthPlan: { value: healthPlanId },
+    } = payload;
+    const data = {
+      percentage,
+      amount,
+      description,
+      requireAuthorizationCode,
+      healthPlanId,
+    };
+    try {
+      const createNHISHealthPlan = createNHISHealthPlanUrl();
+      const createNHISHealthPlanConfig = fetchConfig({
+        url: createNHISHealthPlan,
+        data: data,
+        method: "post",
+      });
+      const res = await fetchWrapper(createNHISHealthPlanConfig);
+      if (res.status === 200) {
+        notification.success({ message: res.data.message });
+        history.push("/AdminManageNHIS");
+      }
+    } catch (error) {
+      notification.error({ message: error?.response?.data.message });
+    }
+    console.log(data);
+  };
+  const { healthPlan } = payload;
   return (
     <>
       <PageLoader />
@@ -15,7 +110,7 @@ export default function CreateInsurance() {
               <div className="col col-md-12">
                 <div className="card border-light">
                   <div className="card-body">
-                    <form className="mb-4 p-5">
+                    <form className="mb-4 p-5" onSubmit={handleSubmit}>
                       <h4 className="text-center">Create NHIS Health Plan</h4>
                       <div className="form-group">
                         <label>Plan Name</label>
@@ -24,6 +119,7 @@ export default function CreateInsurance() {
                           type="text"
                           tabIndex={-98}
                           name="name"
+                          onChange={handleChange}
                           required
                         />
                       </div>
@@ -33,7 +129,8 @@ export default function CreateInsurance() {
                           className="form-control"
                           type="number"
                           tabIndex={-98}
-                          name="name"
+                          name="percentage"
+                          onChange={handleChange}
                           required
                         />
                       </div>
@@ -43,23 +140,31 @@ export default function CreateInsurance() {
                           className="form-control"
                           type="number"
                           tabIndex={-98}
-                          name="name"
+                          name="amount"
+                          onChange={handleChange}
                           required
                         />
                       </div>
                       <div className="form-group">
                         <label>Select Health Plan Type</label>
-                        <select className="form-control" name="workDays">
-                          <option value="">A</option>
-                          <option value="Monday">B</option>
-                        </select>
+                        <Select
+                          value={healthPlan}
+                          onChange={handleSelect}
+                          isSearchable={false}
+                          options={options}
+                          placeholder={
+                            error ? "Sorry, unable to fetch. Retry" : "Search"
+                          }
+                        />
                       </div>
                       <div className="form-group">
                         <label>Description</label>
                         <textarea
+                          name="description"
                           className="form-control"
                           type="text"
                           tabIndex={-98}
+                          onChange={handleChange}
                           required
                         />
                       </div>
@@ -67,6 +172,7 @@ export default function CreateInsurance() {
                         <input
                           type="checkbox"
                           className="custom-control-input"
+                          onChange={handleChange}
                           id="customCheck1"
                         />
                         <label
@@ -79,7 +185,11 @@ export default function CreateInsurance() {
                       <div className="row">
                         <div className="col"></div>
                         <div className="col text-right">
-                          <button type="submit" className="btn btn-primary">
+                          <button
+                            type="submit"
+                            disabled={emptyField ? true : false}
+                            className="btn btn-primary"
+                          >
                             Submit
                           </button>
                         </div>
