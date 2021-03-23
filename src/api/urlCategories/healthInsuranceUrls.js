1ccdd426db38e1f75f79052d49ddbbacba73f7c5
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-23 06:59:39 +0100

    addded url for assigning patients

--- src/api/urlCategories/healthInsuranceUrls.js
+++ src/api/urlCategories/healthInsuranceUrls.js
@@ -7,8 +7,12 @@ export const getHealthPlanDrugsByHealthPlanUrl = (healthPlanId, pageNumber, page
   `/${baseName}/CreateHealthPlanDrug`;
   export const deleteNHISHealthPlanDrugUrl = () =>
   `/${baseName}/DeleteHealthPlanDrug`;
+
+//Health Insurance - Manage NHIS Health Plan Patients
+export const assignPatientToNHISHealthPlanUrl = () =>
+`/${baseName}/AssignPatientToNHISHealthPlan`;
   
-  // Health Insurance - Manage NHIS Health Plan Services
+// Health Insurance - Manage NHIS Health Plan Services
   export const getHealthPlanServicesByHealthPlanUrl = (healthPlanId, pageNumber, pageSize) =>
   `/${baseName}/GetHealthPlanServicesByHealthPlan?HealthPlanId=${healthPlanId}&PageNumber=${pageNumber}&PageSize=${pageSize}`;
   export const createNHISHealthPlanServiceUrl = () =>
