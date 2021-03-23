7714ce660b16ccd99ddf0aad05b1658dbc4b96f1
Nancy Rike <nancyrike7@gmail.com>
2021-03-23 17:11:16 +0100

    work on editHealthPlan

--- src/api/urlCategories/healthInsuranceUrls.js
+++ src/api/urlCategories/healthInsuranceUrls.js
@@ -4,6 +4,7 @@ const baseName = "HealthInsurance";
 export const getHMOHealthPlansUrl = (hmoId, pageNumber, pageSize) =>
   `/${baseName}/GetHMOHealthPlans?HMOId=${hmoId}&PageNumber=${pageNumber}&PageSize=${pageSize}`;
 export const createHMOHealthPlanUrl = () => `/${baseName}/CreateHealthPlan`;
+export const editHMOHealthPlanUrl = () => `/${baseName}/UpdateHealthPlan`;
 
 // Health Insurance - Manage HMOS
 export const getHMOsUrl = (pageNumber, pageSize) =>
