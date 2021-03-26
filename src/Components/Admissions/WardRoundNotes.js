7e4245fcb2fd76a35bfb39475df615612e29e57c
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-26 12:05:33 +0100

    splitted the wardround component into smaller components

--- src/Components/Admissions/WardRoundNotes.js
+++ src/Components/Admissions/WardRoundNotes.js
@@ -2,19 +2,11 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { PageLoader } from "../../Components";
 import { ClarkingHistory } from "../../Components/Clarking";
-import DoctorsNotes from "./ward-round-components/DoctorsNotes";
-import Medications from "./ward-round-components/Medications";
-import { ObservationCharts } from "./ward-round-components/ObservationChart";
 import { useHistory, useParams } from "react-router";
 import { observer } from "mobx-react";
 import { UserContext } from "../../mobx/UserState";
-import {
-  DrugsInInvoice,
-  ServiceRequestsInInvoice,
-} from "./admission-invoices-components";
-import { getAdmissionInvoiceUrl } from "../../api/URLs";
-import { fetchConfig } from "../../api/fetchConfig";
-import { useRequest } from "../../api/fetcher";
+import WardRoundTabHeader from "./ward-round-components/WardRoundTabHeader";
+import WardRoundTabContent from "./ward-round-components/WardRoundTabContent";
 
 const WardRoundNotes = observer(() => {
   const {
@@ -30,17 +22,6 @@ const WardRoundNotes = observer(() => {
       },
     },
   } = useHistory();
-
-  // get admission invoice
-  const invoicesUrl = getAdmissionInvoiceUrl(admissionId);
-  const getAdmissionInvoiceConfig = fetchConfig({
-    url: invoicesUrl,
-    method: "get",
-  });
-  const { data, error } = useRequest(getAdmissionInvoiceConfig, {
-    revalidateOnFocus: false,
-  });
-  if (error) return <div>failed to load</div>;
   return (
     <>
       <PageLoader />
@@ -89,144 +70,9 @@ const WardRoundNotes = observer(() => {
                   aria-labelledby="v-pills-home-tab"
                 >
                   <div>
-                    <ul
-                      className="nav nav-tabs mb-3"
-                      id="pills-tab"
-                      role="tablist"
-                    >
-                      <li className="nav-item">
-                        <a
-                          className="nav-link active"
-                          id="pills-home-tab"
-                          data-toggle="pill"
-                          href="#pills-home"
-                          role="tab"
-                          aria-controls="pills-home"
-                          aria-selected="true"
-                        >
-                          Doctors Notes
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a
-                          className="nav-link"
-                          id="pills-profile-tab"
-                          data-toggle="pill"
-                          href="#pills-profile"
-                          role="tab"
-                          aria-controls="pills-profile"
-                          aria-selected="false"
-                        >
-                          Medications
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a
-                          className="nav-link"
-                          id="pills-contact-tab"
-                          data-toggle="pill"
-                          href="#pills-contact"
-                          role="tab"
-                          aria-controls="pills-contact"
-                          aria-selected="false"
-                        >
-                          Observation Chart
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a
-                          className="nav-link"
-                          id="pills-drug-tab"
-                          data-toggle="pill"
-                          href="#pills-drug"
-                          role="tab"
-                          aria-controls="pills-drug"
-                          aria-selected="false"
-                        >
-                          Drugs administered
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a
-                          className="nav-link"
-                          id="pills-services-tab"
-                          data-toggle="pill"
-                          href="#pills-services"
-                          role="tab"
-                          aria-controls="pills-services"
-                          aria-selected="false"
-                        >
-                          Services administered
-                        </a>
-                      </li>
-                    </ul>
-                    <div className="tab-content" id="pills-tabContent">
-                      <div
-                        className="tab-pane fade show active"
-                        id="pills-home"
-                        role="tabpanel"
-                        aria-labelledby="pills-home-tab"
-                      >
-                        <DoctorsNotes admissionId={admissionId} />
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-profile"
-                        role="tabpanel"
-                        aria-labelledby="pills-profile-tab"
-                      >
-                        <Medications admissionId={admissionId} />
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-contact"
-                        role="tabpanel"
-                        aria-labelledby="pills-contact-tab"
-                      >
-                        <div className="row justify-content-center mt-5">
-                          <div className="col-md-8">
-                            <div className="card border-light m-auto">
-                              <div className="card-body">
-                                <div className="d-flex justify-content-between align-items-center">
-                                  <h4>Observation Chart</h4>
-                                  <button
-                                    className="btn btn-primary"
-                                    to="#"
-                                    data-toggle="modal"
-                                    data-target="#update-observation"
-                                  >
-                                    Update Observation
-                                  </button>
-                                </div>
-
-                                <ObservationCharts admissionId={admissionId} />
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-drug"
-                        role="tabpanel"
-                        aria-labelledby="pills-drug-tab"
-                      >
-                        <DrugsInInvoice
-                          admissionInvoiceId={data?.admissionInvoice.id}
-                        />
-                      </div>
-                      <div
-                        className="tab-pane fade"
-                        id="pills-services"
-                        role="tabpanel"
-                        aria-labelledby="pills-services-tab"
-                      >
-                        <ServiceRequestsInInvoice
-                          admissionInvoiceId={data?.admissionInvoice.id}
-                        />
-                      </div>
-                    </div>
+                    <WardRoundTabHeader />
                   </div>
+                  <WardRoundTabContent admissionId={admissionId} />
                 </div>
               </div>
             </div>
