0be3bfdd39a230f4b847d67babae4b84f37aed31
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-29 10:35:17 +0100

    modified the function

--- src/utils/formatAmount.js
+++ src/utils/formatAmount.js
@@ -1,14 +1,27 @@
 const numberFormatter = (number) => {
   number = String(number);
-  number = number.split("").reverse();
   let newArray = [];
-  number.forEach((element, index) => {
-    if (index % 3 === 0 && index !== 0) {
-      newArray.push(",");
-    }
-    newArray.push(element);
-  });
-  return newArray.reverse().join("");
+  if (number.includes(".")) {
+    let mainNumber = number.split(".")[0];
+    let decimal = number.split(".")[1];
+    number = mainNumber.split("").reverse();
+    number.forEach((element, index) => {
+      if (index % 3 === 0 && index !== 0) {
+        newArray.push(",");
+      }
+      newArray.push(element);
+    });
+    return newArray.reverse().join("") + "." + decimal;
+  } else {
+    number = number.split("").reverse();
+    number.forEach((element, index) => {
+      if (index % 3 === 0 && index !== 0) {
+        newArray.push(",");
+      }
+      newArray.push(element);
+    });
+    return newArray.reverse().join("");
+  }
 };
 
 export default numberFormatter;
