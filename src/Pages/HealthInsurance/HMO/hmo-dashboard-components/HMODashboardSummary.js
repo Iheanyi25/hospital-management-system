06ca6ddf958663718750898f76a61bf27e05607f
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-03-29 16:21:37 +0100

    made the counters dynamic

--- src/Pages/HealthInsurance/HMO/hmo-dashboard-components/HMODashboardSummary.js
+++ src/Pages/HealthInsurance/HMO/hmo-dashboard-components/HMODashboardSummary.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { fetchConfig } from "../../../../api/fetchConfig";
+import { useRequest } from "../../../../api/fetcher";
+import { getHMOCountersUrl } from "../../../../api/URLs";
 import TableSize from "../../../../Components/DataTable/TableSize";
+import { UserContext } from "../../../../mobx/UserState";
 
-const HMODashboardSummary = ({ planCount, userCount, userGroupCount }) => {
+const HMODashboardSummary = () => {
+  const { hmoId } = useContext(UserContext);
+  const getHMOCounters = getHMOCountersUrl(hmoId);
+  const getHMOCountersConfig = fetchConfig({
+    url: getHMOCounters,
+    method: "get",
+  });
+  const { data } = useRequest(getHMOCountersConfig, {
+    revalidateOnFocus: false,
+  });
   return (
     <div className="row">
-      <TableSize size={planCount} heading="Number of Plans" />
-      <TableSize size={userCount} heading="Total Users" />
-      <TableSize size={userGroupCount} heading="Total User Groups" />
+      <TableSize size={data?.hmoHealthPlanCount || 0} heading="Number of Plans" />
+      <TableSize size={data?.hmoHealthPlanPatientCount || 0} heading="Total Users" />
+      <TableSize size={data?.hmoSubUserGroupCount || 0} heading="Total User Groups" />
     </div>
   );
 };
