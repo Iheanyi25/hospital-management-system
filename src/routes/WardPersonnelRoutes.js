427d7c7534ed3495e30224e3732c1d709d8fdf5d
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-04-01 17:47:09 +0100

    routed through more roles

--- src/routes/WardPersonnelRoutes.js
+++ src/routes/WardPersonnelRoutes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../Pages/WardPersonnel/Dashboard";
+import ViewWardPersonnelProfile from "../Pages/WardPersonnel/ViewWardPersonnelProfile";
 import CreateWard from "../Components/Admissions/Wards/CreateWard";
 import EditWard from "../Components/Admissions/Wards/EditWard";
 import ManageWards from "../Components/Admissions/Wards/ViewWards";
-// import ManageBeds from "../Components/Admissions/Wards/ViewBeds";
+import ManageBeds from "../Components/Admissions/Wards/ViewBeds";
+import AssignWard from "../Components/Admissions/Wards/AssignWard";
+import AssignBed from "../Components/Admissions/Wards/AssignBed";
 import { WardPersonnelLayout } from "../Components/Layout";
-import Dashboard from "../Pages/WardPersonnel/Dashboard";
-import ViewWardPersonnelProfile from "../Pages/WardPersonnel/ViewWardPersonnelProfile";
 
 export default function WardPersonnelRoutes() {
   return (
@@ -18,9 +20,9 @@ export default function WardPersonnelRoutes() {
           <Route exact path="/WardCreateWard" component={CreateWard} />
           <Route exact path="/WardEditWard/:id" component={EditWard} />
           <Route exact path="/WardManageWards" component={ManageWards} />
-          {/* <Route exact path="/WardManageBeds/:id" component={ManageBeds} />
+          <Route exact path="/WardManageBeds/:id" component={ManageBeds} />
           <Route exact path="/WardAssignWard/:id" component={AssignWard} />
-          <Route exact path="/WardAssignBed/:id" component={AssignBed} /> */}
+          <Route exact path="/WardAssignBed/:id" component={AssignBed} />
           <Route
             exact
             path="*"
