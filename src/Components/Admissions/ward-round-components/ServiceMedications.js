62d91cffe86daa496085b5d157efba236d42e23c
Nancy Rike <nancyrike7@gmail.com>
2021-04-01 10:28:33 +0100

    fix modal issues

--- src/Components/Admissions/ward-round-components/ServiceMedications.js
+++ src/Components/Admissions/ward-round-components/ServiceMedications.js
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react";
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { fetchConfig } from "../../../api/fetchConfig";
 import { fetchWrapper, useRequest } from "../../../api/fetcher";
 import {
@@ -13,6 +14,7 @@ import { Table } from "../../DataTable";
 import ActionButton from "../../DataTable/ActionButton";
 import { UpdateMedicationStatus } from "../../Modals";
 import UpdateServiceMedication from "../../Modals/UpdateServiceMedication";
+import { DisplayNotes } from "../../Modals/DisplayNotes";
 
 const ServiceMedications = observer(({ admissionId }) => {
   const{
@@ -20,6 +22,10 @@ const ServiceMedications = observer(({ admissionId }) => {
   } = useContext(UserContext);
   const [pageNumber, setPageNumber] = useState(1);
   const [pageSize, setPageSize] = useState(50);
+  const [serviceAdministration, setserviceAdministration] = useState({
+    title:"",
+    body:"",
+  });
   const getServiceMedications = getServiceMedicationsUrl(
     admissionId,
     pageNumber,
@@ -91,6 +97,8 @@ const ServiceMedications = observer(({ admissionId }) => {
             mutate={mutate}
             administerService={administerService}
             serviceId={medication?.serviceId}
+            serviceNotes={`${administrationInstruction ?? "N/A"}`}
+            setserviceAdministration={setserviceAdministration}
           />
         ),
       };
@@ -127,12 +135,13 @@ const ServiceMedications = observer(({ admissionId }) => {
         </div>
       </div>
       <UpdateServiceMedication admissionId={admissionId} mutate={mutate} />
+      <DisplayNotes details={serviceAdministration}/>
       {/* <UpdateMedicationStatus  medicationId={medicationId} mutate={mutate} /> */}
     </div>
   );
 });
 
-const ActionTableAction = ({ id, serviceId, administerService, mutate }) => {
+const ActionTableAction = ({ id, serviceId, administerService, mutate, setserviceAdministration, serviceNotes }) => {
   return (
     <>
       <ActionButton>
@@ -144,6 +153,15 @@ const ActionTableAction = ({ id, serviceId, administerService, mutate }) => {
           <span className="btn-icon icofont-server mr-2" />
           Update status
         </button>
+        <Link
+          data-toggle="modal"
+          data-target="#notes"
+          className="btn btn-sm btn-block"
+          onClick={()=>
+          setserviceAdministration({title: "Administration Instructions", body: serviceNotes})}
+        >
+          Service Medication
+        </Link>
         <button 
         onClick={() => administerService(serviceId)}
         className="btn btn-sm btn-block"
