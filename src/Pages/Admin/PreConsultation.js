1e80359f2d52e6d8cca7085338a5073bdc3124d0
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-04-19 17:00:13 +0100

    refactored and added new validations

--- src/Pages/Admin/PreConsultation.js
+++ src/Pages/Admin/PreConsultation.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 import { fetchConfig } from "../../api/fetchConfig";
-import { fetchWrapper } from "../../api/fetcher";
+import { fetchWrapper, useRequest } from "../../api/fetcher";
 import {
   getPatientUrl,
   updatePatientPreConsultationVitalsUrl,
@@ -8,179 +9,43 @@ import {
 } from "../../api/URLs";
 import { PageLoader } from "../../Components";
 import { notification } from "../../utils/notification";
+import { isValidPositiveInteger } from "../../utils/validationUtils";
 
-class PreConsultation extends React.Component {
-  constructor(props) {
-    super(props);
+const PreConsultation = () => {
+  const { id } = useParams();
+  const getPatient = getPatientUrl(id);
+  const getPatientConfig = fetchConfig({ url: getPatient, method: "get" });
+  const { data, error } = useRequest(getPatientConfig, {
+    revalidateOnFocus: false,
+  });
+  if (error) return <div>failed to load</div>;
+  return (
+    <>
+      <PageLoader />
 
-    this.state = {
-      patientId: "",
-      patient: null,
-
-      bloodPressure: "",
-      respiration: "",
-      pulse: "",
-      spo2: "",
-      temperature: "",
-
-      weight: "",
-      height: "",
-      calculatedBMI: "",
-    };
-  }
-
-  async componentDidMount() {
-    const { params } = this.props.match;
-    await this.setState({ patientId: params.id });
-    const getPatient = getPatientUrl(this.state.patientId);
-    const getPatientConfig = fetchConfig({ url: getPatient, method: "get" });
-    const { data } = await fetchWrapper(getPatientConfig);
-
-    console.log(data, 111111);
-    this.setState({
-      patient: data,
-    });
-  }
-
-  handleChange = async (name, e) => {
-    const value = e.target.value;
-    await this.setState({
-      [name]: value,
-    });
-    if (this.state.weight !== "" && this.state.height !== "") {
-      this.setState({
-        calculatedBMI: parseFloat(
-          this.state.weight / Math.pow(this.state.height, 2)
-        ).toFixed(2),
-      });
-    }
-  };
-
-  updatePatientBMI = async (e) => {
-    e.preventDefault();
-
-    try {
-      const payload = {
-        weight: this.state.weight,
-        height: this.state.height,
-        calculatedBMI: this.state.calculatedBMI,
-        patientId: this.state.patientId,
-      };
-      const updatePatientPreConsultationBMI = updatePatientPreConsultationBMIUrl();
-      const updatePatientPreConsultationBMIConfig = fetchConfig({
-        url: updatePatientPreConsultationBMI,
-        data: payload,
-        method: "post",
-      });
-      const res = await fetchWrapper(updatePatientPreConsultationBMIConfig);
-      notification.success({ message: res.data.message });
-    } catch (error) {
-      console.log(error);
-      notification.error({ message: error?.response?.data.message });
-    }
-  };
-
-  render() {
-    const {
-      patient,
-
-      weight,
-      height,
-      calculatedBMI,
-    } = this.state;
-    return (
-      <>
-        <PageLoader />
-
-        {patient ? (
-          <main className="main-content mt-5">
-            <div className="app-loader">
-              <i className="icofont-spinner-alt-4 rotate" />
-            </div>
-            <div className="main-content-wrap">
-              <header className="page-header">
-                <h3 className="page-title">
-                  {`Patient Preconsultation (${patient?.patient?.firstName} ${patient?.patient?.lastName})`}
-                </h3>
-              </header>
-              <div className="page-content">
-                <div className="row justify-content-center">
-                  <UpdatePatientVitalsForm
-                    patientId={this.props.match.params.id}
-                  />
-                  <div className="col col-md-6">
-                    <div className="card border-light">
-                      <div className="card-body">
-                        <form className="mb-4">
-                          <h4>Patient BMI</h4>
-                          <div className="row">
-                            <div className="col-12 col-sm-6">
-                              <div className="form-group">
-                                <label>Weigth(Kg)</label>
-                                <input
-                                  className="form-control"
-                                  type="number"
-                                  min="0"
-                                  value={weight}
-                                  onChange={(e) =>
-                                    this.handleChange("weight", e)
-                                  }
-                                />
-                              </div>
-                            </div>
-                            <div className="col-12 col-sm-6">
-                              <div className="form-group">
-                                <label>Height(M)</label>{" "}
-                                <input
-                                  className="form-control"
-                                  type="number"
-                                  min="0"
-                                  value={height}
-                                  onChange={(e) =>
-                                    this.handleChange("height", e)
-                                  }
-                                />
-                              </div>
-                            </div>
-                          </div>
-
-                          <div className="form-group">
-                            <label>Calculated BMI</label>{" "}
-                            <input
-                              className="form-control"
-                              type="number"
-                              value={calculatedBMI}
-                              onChange={(e) =>
-                                this.handleChange("calculatedBMI", e)
-                              }
-                              disabled
-                            />
-                          </div>
-
-                          <div className="row mt-5 justify-content-end">
-                            <div className="col text-right">
-                              <button
-                                type="button"
-                                className="btn btn-primary"
-                                onClick={(e) => this.updatePatientBMI(e)}
-                              >
-                                Save Patient BMI
-                              </button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+      {data && (
+        <main className="main-content mt-5">
+          <div className="app-loader">
+            <i className="icofont-spinner-alt-4 rotate" />
+          </div>
+          <div className="main-content-wrap">
+            <header className="page-header">
+              <h3 className="page-title">
+                {`Patient Preconsultation (${data?.patient?.firstName} ${data?.patient?.lastName})`}
+              </h3>
+            </header>
+            <div className="page-content">
+              <div className="row justify-content-center">
+                <UpdatePatientVitalsForm patientId={id} />
+                <UpdatePatientBMIForm patientId={id} />
               </div>
             </div>
-          </main>
-        ) : null}
-      </>
-    );
-  }
-}
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
 
 export default PreConsultation;
 
@@ -231,7 +96,14 @@ const UpdatePatientVitalsForm = ({ patientId }) => {
       notification.error({ message: error?.response?.data.message });
     }
   };
-  const { systolic, diastolic, respiration, pulse, spo2, temperature } = details;
+  const {
+    systolic,
+    diastolic,
+    respiration,
+    pulse,
+    spo2,
+    temperature,
+  } = details;
   return (
     <div className="col-md-6">
       <div className="card border-light">
@@ -330,6 +202,118 @@ const UpdatePatientVitalsForm = ({ patientId }) => {
                   onClick={(e) => updatePatientVitals(e)}
                 >
                   Save Patient Vitals
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const UpdatePatientBMIForm = ({ patientId }) => {
+  const [details, setDetails] = useState({
+    weight: "",
+    height: "",
+    calculatedBMI: "",
+  });
+  const [emptyField, setEmptyField] = useState(true);
+  useEffect(() => {
+    const { weight, height, calculatedBMI } = details;
+    if (
+      isValidPositiveInteger(weight) &&
+      isValidPositiveInteger(height) &&
+      isValidPositiveInteger(calculatedBMI)
+    ) {
+      setEmptyField(false);
+    } else {
+      setEmptyField(true);
+    }
+  }, [details]);
+  const handleChange = (name, e) => {
+    const value = e.target.value;
+    console.log(name, value, details.calculatedBMI);
+    setDetails({
+      ...details,
+      [name]: value,
+      calculatedBMI: parseFloat(
+        details.weight / Math.pow(details.height, 2)
+      ).toFixed(2),
+    });
+  };
+  const updatePatientBMI = async (e) => {
+    e.preventDefault();
+
+    try {
+      const payload = { weight, height, calculatedBMI, patientId };
+      const updatePatientPreConsultationBMI = updatePatientPreConsultationBMIUrl();
+      const updatePatientPreConsultationBMIConfig = fetchConfig({
+        url: updatePatientPreConsultationBMI,
+        data: payload,
+        method: "post",
+      });
+      const res = await fetchWrapper(updatePatientPreConsultationBMIConfig);
+      notification.success({ message: res.data.message });
+    } catch (error) {
+      console.log(error);
+      notification.error({ message: error?.response?.data.message });
+    }
+  };
+  const { weight, height, calculatedBMI } = details;
+  return (
+    <div className="col col-md-6">
+      <div className="card border-light">
+        <div className="card-body">
+          <form className="mb-4">
+            <h4>Patient BMI</h4>
+            <div className="row">
+              <div className="col-12 col-sm-6">
+                <div className="form-group">
+                  <label>Weigth (Kg)</label>
+                  <input
+                    className="form-control"
+                    type="number"
+                    min="0"
+                    value={weight}
+                    onChange={(e) => handleChange("weight", e)}
+                  />
+                </div>
+              </div>
+              <div className="col-12 col-sm-6">
+                <div className="form-group">
+                  <label>Height (M)</label>{" "}
+                  <input
+                    className="form-control"
+                    type="number"
+                    min="0"
+                    value={height}
+                    onChange={(e) => handleChange("height", e)}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>Calculated BMI</label>{" "}
+              <input
+                className="form-control"
+                type="number"
+                value={calculatedBMI}
+                onChange={(e) => handleChange("calculatedBMI", e)}
+                disabled
+              />
+            </div>
+
+            <div className="row mt-5 justify-content-end">
+              <div className="col text-right">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  disabled={emptyField ? true : false}
+                  onClick={(e) => updatePatientBMI(e)}
+                >
+                  Save Patient BMI
                 </button>
               </div>
             </div>
