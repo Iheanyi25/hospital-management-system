b2523488e30f0cb810e344061bf4b960a8346976
iheanyi10 <i.ihekweaba@genesystechhub.com>
2021-04-20 13:49:05 +0100

    readjusted the UI arrangement

--- src/Pages/Admin/NursingReport/CreateNursingReport.js
+++ src/Pages/Admin/NursingReport/CreateNursingReport.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
-import Select from "react-select";
 import { PageLoader } from "../../../Components";
+import { ShiftSelectionForm } from "./nursing-report-components/forms";
 import NursingReportTabContent from "./nursing-report-components/NursingReportTabContent";
 import NursingReportTabHeader from "./nursing-report-components/NursingReportTabHeader";
 
@@ -33,47 +33,3 @@ const CreateNursingReport = () => {
 };
 
 export default CreateNursingReport;
-
-const ShiftSelectionForm = () => {
-  return (
-    <form className="mb-4">
-      <div className="form-row">
-        <div className="form-group col-12 col-md-8">
-          <label>Shift selection</label>
-          <Select
-            //   value={patient}
-            isSearchable={false}
-            options={[
-              { value: "Morning", label: "Morning" },
-              { value: "Evening", label: "Evening" },
-            ]}
-            //   onChange={handleChange}
-            placeholder="Select shift"
-          />
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group col-md-8">
-          <label htmlFor="inputEmail4">Date</label>
-          <input
-            type="date"
-            name="startYear"
-            className="form-control"
-            // onChange={handleChange}
-            placeholder="eg. 1990"
-          />
-        </div>
-        <div className="form-group col-md-4">
-          <label htmlFor="inputPassword4">Time</label>
-          <input
-            type="time"
-            name="endYear"
-            className="form-control"
-            // onChange={handleChange}
-            placeholder="eg. 1990"
-          />
-        </div>
-      </div>
-    </form>
-  );
-};
