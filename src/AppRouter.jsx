d170db86cca9612e5a193352bd0eabe0167baecb
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2023-02-23 14:23:01 +0100

    fixed issues with routing

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-import ViewResultSheet from "./pages/resultSheet/ViewResultSheet";
+import ViewResultSheet from "./pages/resultSheet/viewResultSheet";
 import { MainRouter } from "./routers/mainRouter";
 
 const AppRouter = () => {
